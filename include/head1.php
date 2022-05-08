0756adcb2669f0d57987911cf25d96c9dc627cc1
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-08 21:47:17 +0200

    Initial

---
+++ include/head1.php
@@ -0,0 +1,6 @@
+<head>
+        <title>mwiese.de</title>
+        <link rel="shortcut icon" href="https://mwiese.de/45454.ico" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style.css">
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    </head>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "https://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <?php include ("include/head1.php"); ?>
+    <body>
+	    <?php include ("include/body1.php"); ?>
+        <div class="center1">
+                <h1>Informationen zur Seite</h1>
+            <br />
+            <br />
+                <p class="info001">Diese Seite ist eine art Fun-Projekt und daher kann es sein das es dauert bis neues hinzugefügt wird.<br />
+				Zusätzlich sei gesagt, das diese Website von sich aus keine Browserdaten der Nutzer erhebt.<br />
+				Das einzige was der Provider dieser Domain (Lima-City.de) an Daten erhebt ist die anzahl der einzelnen Seitenaufrufe pro Woche.<br />
+				Sollten auf nachfolgenden Seiten anderweitig Daten erhoben werden, weise ich darauf hin.</p>
+            <br />
+                <div class="spass"><p>Aber vor allem gilt natürlich:<br />Viel Spaß auf meiner seite :)</p></div>
+            <br />
+            <br />
+                <a class="link001" href="mobile/index.php">mobile-site</a>
+            <br />
+            <br />
+                <a class="link001" href="Startpage.php">Zur Startseite</a>
+            <br />
+        </div>
+    </body>
+</html>
