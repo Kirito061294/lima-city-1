47bfde23bc5500f2d1a177e7f410d4fcfd3ae1c4
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-10 15:48:01 +0200

    Add files via upload

--- include/head1.php
+++ include/head1.php
@@ -1,6 +1,26 @@
-<head>
-        <title>mwiese.de</title>
-        <link rel="shortcut icon" href="https://mwiese.de/45454.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="https://mwiese.de/include/45454.ico" type="image/x-icon"/>
         <link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style.css">
         <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     </head>
+	<body>
+        <header>
+            <section id="navigation">
+                <nav id="menu">
+                    <ul class="kopf">
+                        <li><a href="https://mwiese.de/Startpage.php">Startseite</a></li>
+                        <li><a href="https://pc.mwiese.de">Mein Hobby</a></li>
+                        <li><a href="https://mwiese.de/U/U.php">Unterhaltung</a>
+						    <ul class="untermenue">
+							    <li><a href="https://mwiese.de/F/F.php">Filme und Videos</a></li>
+								<li><a href="https://mwiese.de/M/M.php">Musik</a></li>
+								<li><a href="https://mwiese.de/S/S.php">Spiele</a></li>
+							</ul>
+						</li>
+                        <li><a href="https://mwiese.de/T/index.php">Technik</a></li>
+                        <li><a href="https://mwiese.de/N/N.php">Internet/Netzwerk</a></li>
+                        <li><a href="https://mwiese.de/I.php">Impressum</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </header>
+        <div class="center1">
