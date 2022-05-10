577678245d48342a7f12c9c9aee1596596c6e4d9
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-10 15:45:48 +0200

    Delete head.php

--- include/head.php
+++
@@ -1,3 +0,0 @@
-<link rel="shortcut icon" href="http://mwiese.de/45454.ico" type="image/x-icon"/>
-        <link rel="stylesheet" type="text/css" href="http://mwiese.de/style.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
