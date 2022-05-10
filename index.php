e848846e4c0e4b53eb78cc92efa0a616bd16e610
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-10 15:49:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,26 +2,20 @@
        "https://www.w3.org/TR/html4/loose.dtd">
 
 <html>
-    <?php include ("https://mwiese.de/include/head1.php"); ?>
-    <body>
-	    <?php include ("https://mwiese.de/include/body1.php"); ?>
-        <div class="center1">
-                <h1>Informationen zur Seite</h1>
-            <br />
-            <br />
-                <p class="info001">Diese Seite ist eine art Fun-Projekt und daher kann es sein das es dauert bis neues hinzugefügt wird.<br />
-				Zusätzlich sei gesagt, das diese Website von sich aus keine Browserdaten der Nutzer erhebt.<br />
-				Das einzige was der Provider dieser Domain (Lima-City.de) an Daten erhebt ist die anzahl der einzelnen Seitenaufrufe pro Woche.<br />
+    <head>
+	    <title>mwiese.de</title>
+    <?php include ("include/head1.php"); ?>
+            <div class="ueberschrift">
+			    <h1>Informationen zur Seite</h1>
+			</div>
+                <p class="info001">Diese Seite ist eine art Fun-Projekt und daher kann es sein das es dauert bis neues hinzugefügt wird.
+				Zusätzlich sei gesagt, das diese Website von sich aus keine Browserdaten der Nutzer erhebt.
+				Das einzige was der Provider dieser Domain (Lima-City.de) an Daten erhebt ist die anzahl der einzelnen Seitenaufrufe pro Woche.
 				Sollten auf nachfolgenden Seiten anderweitig Daten erhoben werden, weise ich darauf hin.</p>
-            <br />
-                <div class="spass"><p>Aber vor allem gilt natürlich:<br />Viel Spaß auf meiner seite :)</p></div>
-            <br />
-            <br />
-                <a class="link001" href="mobile/index.php">mobile-site</a>
-            <br />
-            <br />
-                <a class="link001" href="Startpage.php">Zur Startseite</a>
-            <br />
+            <div class="spass">
+			    <p>Aber vor allem gilt natürlich:<br />Viel Spaß auf meiner seite :)</p>
+				<p><a class="link001" href="mobile/index.php">mobile-site</a> - <a class="link001" href="Startpage.php">Zur Startseite</a></p>
+			</div>
         </div>
     </body>
 </html>
