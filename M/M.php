fcaa8a6d34f71ccd96b02a402cf2179dce810781
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-11 18:07:52 +0200

    Add files via upload

---
+++ M/M.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+
+
+<html>
+    <head>
+        <title>Musik</title>
+        <?php include ("../include/head1.php"); ?>
+            <div class="ueberschrift">
+                <h1>Musik</h1>
+			</div>
+			<div class="info001">
+                Die auf den folgenden Seiten eingefügten Videos verweisen auf YouTube.com.
+				Aus diesem Grund muss ich auf die <a href="https://www.youtube.com/t/terms">Nutzungsbedingungen von YouTube</a> hinweisen.
+            </div>
+                <p>____________________________________________</p>
+            <div class="linkzone01">
+                <p><a href="A-T.php">Adel Tawil</a></p>
+                <p><a href="A-W.php">Alan Walker</a></p>
+                <p><a href="a2.php">Anderes</a></p>
+                <p><a href="A_i.php">Anime Tracks</a></p>
+                <p><a href="ds.php">Damestream</a></p>
+                <p><a href="gd.php">Geometry Dash</a></p>
+                <p><a href="G-E.php">George Ezra</a></p>
+                <p><a href="N.php">Nightcore</a></p>
+                <p><a href="S-A.php">Sunrise Avenue</a></p>
+            </div>
+                <p>____________________________________________</p>
+            <div class="ende1">
+                <a href="../U/U.php">Zurück</a>
+            </div>
+		</div>
+    </body>
+</html>
