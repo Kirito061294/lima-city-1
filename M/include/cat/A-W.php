3c32eb5514500dceebce58554f89698ece35c717
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-11 18:09:10 +0200

    Add files via upload

---
+++ M/include/cat/A-W.php
@@ -0,0 +1,12 @@
+			<form action="" method="post" class="mb-3">
+			    <select name="alan">
+                    <option value=""></option>
+                    <option value="alan001">1</option>
+                    <option value="alan002">1</option>
+                    <option value="alan003">1</option>
+                    <option value="alan004">1</option>
+                    <option value="alan005">1</option>
+                    <option value="alan006">1</option>
+		        </select>
+                <input type="submit" name="submit" value="Auswahl">
+            </form>
---
+++ M/include/cat/a2.php
@@ -0,0 +1,11 @@
+
+                <p><a href="P/a2/2009.php">2009</a></p>
+                <p><a href="P/a2/2010.php">2010</a></p>
+                <p><a href="P/a2/2011.php">2011</a></p>
+                <p><a href="P/a2/2012-1.php">2012 (1)</a></p>
+                <p><a href="P/a2/2012-2.php">2012 (2)</a></p>
+                <p><a href="P/a2/2013-1.php">2013 (1)</a></p>
+                <p><a href="P/a2/2013-2.php">2013 (2)</a></p>
+                <p><a href="P/a2/2014.php">2014</a></p>
+                <p><a href="P/a2/2015.php">2015</a></p>
+				<p><a href="P/a2/2016.php">2016</a></p>
