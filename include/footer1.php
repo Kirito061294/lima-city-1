e19832fec89570bf91100f5e0b6582c49ea60a99
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-13 19:51:40 +0200

    Add files via upload

---
+++ include/footer1.php
@@ -0,0 +1,10 @@
+<p>____________________________________________</p>
+            <div class="ende1">
+                <a href="javascript:history.back()">Zurück</a>
+            </div>
+		</div>
+    </body>
+	<footer>
+        <p>Author: Marcel Wiese</p>
+        <p>Copyright: mwiese.de 2018 - 2022</p>
+    </footer>
