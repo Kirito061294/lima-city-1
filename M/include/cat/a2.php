4b5ad11ae86d107a50e5ab00953ceb8ca8cbf7d8
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-13 19:49:13 +0200

    Update a2.php

--- M/include/cat/a2.php
+++ M/include/cat/a2.php
@@ -1,11 +1,15 @@
-
-                <p><a href="P/a2/2009.php">2009</a></p>
-                <p><a href="P/a2/2010.php">2010</a></p>
-                <p><a href="P/a2/2011.php">2011</a></p>
-                <p><a href="P/a2/2012-1.php">2012 (1)</a></p>
-                <p><a href="P/a2/2012-2.php">2012 (2)</a></p>
-                <p><a href="P/a2/2013-1.php">2013 (1)</a></p>
-                <p><a href="P/a2/2013-2.php">2013 (2)</a></p>
-                <p><a href="P/a2/2014.php">2014</a></p>
-                <p><a href="P/a2/2015.php">2015</a></p>
-				<p><a href="P/a2/2016.php">2016</a></p>
+<form action="" method="post" class="mb-3">
+			    <select name="anderes">
+                    <option value="" disabled selected></option>
+                    <option value="anderes">2009</option>
+                    <option value="anderes">2010</option>
+                    <option value="anderes">2011</option>
+                    <option value="anderes">2012 (1)</option>
+                    <option value="anderes">2012 (2)</option>
+                    <option value="anderes">2013 (1)</option>
+                    <option value="anderes">2013 (2)</option>
+                    <option value="anderes">2014</option>
+                    <option value="anderes">2015</option>
+				    <option value="anderes">2016</option>
+		        </select>
+            </form>
