26d9aedd599a429553d0f177562641b4fdcfa7b6
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-13 19:42:09 +0200

    Update A_i.php

--- M/include/cat/A_i.php
+++ M/include/cat/A_i.php
@@ -1,13 +1,13 @@
-
-                <p><a href="https://youtu.be/9bK7vyyW4v0">Accel World Opening 1</a></p>
-                <p><a href="P/A_i/AW_ed_1.php">Accel World Ending 1</a></p>
-                <p><a href="P/A_i/A_op_1.php">Amnesia Opening 1</a></p>
-                <p><a href="P/A_i/A_op_1_full.php">Amnesia Opening 1 full</a></p>
-                <p>Fairy Tail Openings</p>
-                <p><a href="P/A_i/FT/1.php">1</a> - <a href="P/A_i/FT/2.php">2</a> - <a href="P/A_i/FT/3.php">3</a> - <a href="P/A_i/FT/4.php">4</a> - <a href="P/A_i/FT/5.php">5</a> - <a href="P/A_i/FT/6.php">6</a> - <a href="P/A_i/FT/7.php">7</a> - <a href="P/A_i/FT/8.php">8</a> - <a href="P/A_i/FT/9.php">9</a> - <a href="P/A_i/FT/10.php">10</a> - <a href="P/A_i/FT/11.php">11</a> - <a href="P/A_i/FT/12.php">12</a> - <a href="P/A_i/FT/13.php">13</a> - <a href="P/A_i/FT/14.php">14</a> - <a href="P/A_i/FT/15.php">15</a> - <a href="P/A_i/FT/16.php">16</a> - <a href="P/A_i/FT/17.php">17</a></p>
-                <p>Pokemon Openings</p>
-                <p><a href="P/A_i/Pkmn/1.php">1</a> - <a href="P/A_i/Pkmn/2.php">2</a> - <a href="P/A_i/Pkmn/3.php">3</a> - <a href="P/A_i/Pkmn/4.php">4</a> - <a href="P/A_i/Pkmn/5.php">5</a> - <a href="P/A_i/Pkmn/6.php">6</a> - <a href="P/A_i/Pkmn/7.php">7</a> - <a href="P/A_i/Pkmn/8.php">8</a> - <a href="P/A_i/Pkmn/9.php">9</a> - <a href="P/A_i/Pkmn/10.php">10</a> - <a href="P/A_i/Pkmn/11.php">11</a> - <a href="P/A_i/Pkmn/12.php">12</a> - <a href="P/A_i/Pkmn/13.php">13</a> - <a href="P/A_i/Pkmn/14.php">14</a> - <a href="P/A_i/Pkmn/15.php">15</a> - <a href="P/A_i/Pkmn/16.php">16</a> - <a href="P/A_i/Pkmn/17.php">17</a></p>
-                <p><a href="P/A_i/LSdAF_op_1.php">La Storia Della Arcana Famiglia Opening 1</a></p>
-                <p><a href="P/A_i/LSdAF_op_1_full.php">La Storia Della Arcana Famiglia Opening 1 full</a></p>
-                <p><a href="P/A_i/MtgIKKSDy_op_1.php">Mondaiji-tachi ga Isekai Kara Kuru Sou Desu yo Opening 1</a></p>
-                <p><a href="P/A_i/STnWB_op_1.php">Seiken Tsukai no World Break Opening 1</a></p>
+<form action="" method="post" class="mb-3">
+			    <select name="anime">
+                    <option value="" disabled selected></option>
+                    <option value="anime001">Accel World Opening 1</option>
+                    <option value="anime002">Accel World Ending 1</option>
+                    <option value="anime003">Amnesia Opening 1</option>
+                    <option value="anime004">Amnesia Opening 1 full</option>
+                    <option value="anime005">La Storia Della Arcana Famiglia Opening 1</option>
+                    <option value="anime006">La Storia Della Arcana Famiglia Opening 1 full</option>
+                    <option value="anime007">Mondaiji-tachi ga Isekai Kara Kuru Sou Desu yo Opening 1</option>
+                    <option value="anime008">Seiken Tsukai no World Break Opening 1</option>
+		        </select>
+            </form>
