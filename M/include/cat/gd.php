671faa69e5d231546a7bb2be7a1b125ce0bf2a57
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-05-13 19:50:04 +0200

    Update gd.php

--- M/include/cat/gd.php
+++ M/include/cat/gd.php
@@ -1,2 +1,6 @@
-
-                <p><a href="">*Leer*</a></p>
+<form action="" method="post" class="mb-3">
+			    <select name="musik">
+                    <option value="" disabled selected></option>
+                    <option value="">*Leer*</option>
+		        </select>
+            </form>
