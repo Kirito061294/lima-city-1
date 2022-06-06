e80a279b4ec1514d1108dd4503da39661b4541ec
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-06-06 21:17:13 +0200

    Update head1.php

--- include/head1.php
+++ include/head1.php
@@ -1,33 +1,46 @@
-    <link rel="shortcut icon" href="https://mwiese.de/include/45454.ico" type="image/x-icon"/>
+<link rel="shortcut icon" href="https://mwiese.de/include/45454.ico" type="image/x-icon"/>
         <link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style.css">
         <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     </head>
 	<body>
         <header>
-            <section id="navigation">
-                <nav id="menu">
+            <section>
+                <nav>
+				    <a class="logo" href="https://mwiese.de/Technik/test.php">mwiese.de</a>
+				    <input type="checkbox" id="toggle_button">
+					<label for="toggle_button" class="toggle_button">
+					    <span class="bar"></span>
+					    <span class="bar"></span>
+					    <span class="bar"></span>
+					</label>
                     <ul class="kopf">
-                        <li><a href="https://mwiese.de">Startseite</a></li>
-                        <li><a href="https://mwiese.de/U/U.php">Unterhaltung</a>
-						    <ul class="untermenue">
-								<li><a href="https://mwiese.de/M/M.php">Musik</a></li>
-								<li><a href="https://mwiese.de/S/S.php">Spiele</a></li>
+                        <li class="expandable_li">
+						    <input type="checkbox" id="unterhaltung_checkbox">
+							<label for="unterhaltung_checkbox">Unterhaltung</label>
+						    <ul class="dropdown">
+								<li><a href="https://mwiese.de/Musik/M.php">Musik</a></li>
+								<li><a href="https://mwiese.de/Unterhaltung/Spiele/Spiele.php">Spiele</a></li>
 							</ul>
 						</li>
-                        <li><a href="https://mwiese.de/T/index.php">Technik</a>
-						    <ul class="untermenue">
-								<li><a href="https://mwiese.de/OS/OS.php">Betriebssysteme</a></li>
-								<li><a href="https://mwiese.de/N/N.php">Internet/Netzwerk</a></li>
-								<li><a href="https://mwiese.de/T/I.php">Interessantes</a></li>
+                        <li class="expandable_li">
+						    <input type="checkbox" id="technik_checkbox">
+							<label for="technik_checkbox">Technik</label>
+						    <ul class="dropdown">
+								<li><a href="https://mwiese.de/Technik/OS/OS.php">Betriebssysteme</a></li>
+								<li><a href="https://mwiese.de/Technik/Netzwerk/Netzwerk.php">Internet/Netzwerk</a></li>
+								<li><a href="https://mwiese.de/Technik/Interessantes.php">Interessantes</a></li>
 							</ul>
 						</li>
-                        <li><a href="">Unterseiten</a>
-						    <ul class="untermenue">
+                        <li class="expandable_li">
+						    <input type="checkbox" id="subdomains_checkbox">
+							<label for="subdomains_checkbox">Unterseiten</label>
+						    <ul class="dropdown">
 								<li><a href="https://pc.mwiese.de" target="_blank">Mein Hobby</a></li>
-								<li><a href="https://themen.mwiese.de" target="_blank">Spezielle Themen</a></li>
+								<li><a href="https://mwiese.de/Themen/Themen.php">Spezielle Themen</a></li>
 							</ul>
 						</li>
-                        <li><a href="https://mwiese.de/I.php">Impressum</a></li>
+                        <li><a href="https://mwiese.de/Rechtliches/Impressum.php">Impressum</a></li>
+                        <li><a href="https://mwiese.de/Rechtliches/Datenchutz.php">Datenschutz</a></li>
                     </ul>
                 </nav>
             </section>
