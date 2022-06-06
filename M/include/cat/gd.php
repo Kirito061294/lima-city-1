c247c6f023d6ae0e5bf15a48a964fbde33288339
Kirito061294 <105172961+Kirito061294@users.noreply.github.com>
2022-06-06 21:18:05 +0200

    Delete M directory

--- M/include/cat/gd.php
+++
@@ -1,6 +0,0 @@
-<form action="" method="post" class="mb-3">
-			    <select name="musik">
-                    <option value="" disabled selected></option>
-                    <option value="">*Leer*</option>
-		        </select>
-            </form>
