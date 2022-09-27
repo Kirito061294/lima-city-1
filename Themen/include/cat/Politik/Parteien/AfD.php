30b69dc105e4e7260b42182ab51eec016acc17b1
Kirito061294 <login@mwiese.de>
2022-09-27 17:29:04 +0200

    Create AfD.php

---
+++ Themen/include/cat/Politik/Parteien/AfD.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <title>Parteien</title>
+        <?php include ("../../../include/head.php"); ?>
+    </head>
+    <body>
+	    <?php include ("../../../include/header.php"); ?>
+	    <div align="center">
+                <h1>Wilkommen!</h1>
+            <br />
+            <br />
+                <h2>
+				</h2>
+            <br />
+            <br />
+            <br />
+                <h3><a href="../index.php">Zur&uuml;ck</a></h3>
+            <br />
+        </div>
+    </body>
+</html>
