a7950f0d57f32e30f010956d6dfe201470169cc9
Kirito061294 <login@mwiese.de>
2022-09-27 17:26:18 +0200

    Create Akti.php

---
+++ Technik/OS/MS/7/Akti.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+      "http://www.w3.org/TR/html4/loose.dtd">
+
+
+<html>
+    <head>
+        <title>Windows 7 Tricks</title>
+        <?php include ("../../../../include/head1.php"); ?>
+            <div class="ueberschrift">
+                <h1>Win7 Aktivierung umgehen</h1>
+			</div>
+                <p>____________________________________________</p>
+            <div class="linkzone01">
+			    <p></p>
+            </div>
+                <?php include ("../../../../include/footer1.php"); ?>
+</html>
