8805557e407cb2f52df90f9291dc7941be371982
Kirito061294 <login@mwiese.de>
2022-09-27 17:26:10 +0200

    Create Ubuntu.php

---
+++ Technik/OS/Linux/Ubuntu.php
@@ -0,0 +1,37 @@
+<p>Offizielle Derivate</p>
+<p>Ubuntu | <a href="" target="_blank">Gnome</a> - <a href="" target="_blank">Budgie</a> - <a href="" target="_blank">MATE</a></p>
+			<p><a href="" target="_blank">Ubuntu Studio</a></p>
+			<p><a href="" target="_blank">Ubuntu Server</a></p>
+			<p><a href="" target="_blank">Kubuntu</a></p>
+			<p><a href="" target="_blank">Xubuntu</a></p>
+			<p><a href="" target="_blank">Lubuntu</a></p>
+			<div class="sub-mod">
+				<p>Abwandlungen</p>
+				<p><a href="" target="_blank">Artistix</a></p>
+				<p><a href="" target="_blank">Bodhi</a></p>
+				<p><a href="" target="_blank">Comfusion</a></p>
+				<p><a href="" target="_blank">dyne:bolic</a></p>
+				<p><a href="" target="_blank">elementary OS</a></p>
+				<p><a href="" target="_blank">Emmabuntüs</a></p>
+				<p><a href="" target="_blank">Feren OS</a></p>
+				<p><a href="" target="_blank">Goobuntu</a></p>
+				<p><a href="" target="_blank">Joli OS</a></p>
+				<p><a href="" target="_blank">Karoshi</a></p>
+				<p><a href="" target="_blank">LiMux</a></p>
+				<p><a href="" target="_blank">Linux Lite</a></p>
+				<p><a href="" target="_blank">LuninuX OS</a></p>
+				<p><a href="" target="_blank">LXLE</a></p>
+				<p><a href="" target="_blank">Mint</a></p>
+				<p><a href="" target="_blank">Peppermint OS</a></p>
+				<p><a href="" target="_blank">Pop! OS</a></p>
+				<p><a href="" target="_blank">Poseidon</a></p>
+				<p><a href="" target="_blank">Puredyne</a></p>
+				<p><a href="" target="_blank">Qimo</a></p>
+				<p><a href="" target="_blank">Super OS</a></p>
+				<p><a href="" target="_blank">Symphony OS</a></p>
+				<p><a href="" target="_blank">Ubuntu Privacy Remix</a></p>
+				<p><a href="" target="_blank">VoiceBuntu</a></p>
+				<p><a href="" target="_blank">Zentyal</a></p>
+				<p><a href="" target="_blank">ZevenOS</a></p>
+				<p><a href="" target="_blank">Zorin OS</a></p>
+			</div>
