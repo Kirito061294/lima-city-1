7c70bb5ea482f30ea4f8d6eecb747779d89f0234
Kirito061294 <login@mwiese.de>
2022-09-27 17:27:46 +0200

    Create index.php

---
+++ Themen/include/cat/Covid-19/0000/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "https://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <title>Covid-19</title>
+        <?php include ("../../include/head.php"); ?>
+    </head>
+    <body>
+	    <?php include ("../../include/header.php"); ?>
+	    <div align="center">
+                <h1>Wilkommen!</h1>
+            <br />
+            <br />
+            <br />
+                <h2>
+				    Kurzinfo: ALLE Informationen sind Fakt und mehrfach bestätigt!<br /><br />
+				</h2>
+				<h3>
+				    Covid-Impfstoffe werden zum Teil mit Zellen produziert,<br />die von abgetriebenen Baby´s stammen<br />
+					<a href="1">Quelle 1</a> - <a href="2">Quelle 2</a> - <a href="3">Quelle 3</a><br />
+					<br /><br />
+					--------------------------------------------------------------
+					<br /><br />
+					Sollte IRGENDJEMAND denken, "Das (Föten, Embrionen) sind doch keine Menschen, sondern nen haufen weiterentwickelter Zellen..."<br />
+					Bedenken Sie bitte: wenn auf dem Mars EIN-ZELLER, also, nicht mit dem Auge sichtbare Lebewesen, als eben Lebewesen klassifiziert werden,<br />
+					WAS gibt den Menschen das Recht, Föten, oder generell ungeborenes Leben nicht als solches zu bezeichnen?<br />
+					<br /><br />
+					--------------------------------------------------------------
+					<br /><br />
+					<a href="0000">Nebenwirkungen, Spätfolgen und andere allgemeine Negativ-infos zur Covid-Lage</a><br />
+					<br /><br />
+					--------------------------------------------------------------
+					<br /><br />
+				    Ich berufe mich hiermit auf mein, durch das Grundgesetz geltendes Recht auf freie Meinungsäußerung.<br />
+				    Sämtliche auf nachfolgenden(!) Seiten beschriebenden Meinungen stammen nicht von mir, ich teile lediglich ihre Ansicht.<br />
+				</h3>
+            <br />
+            <br />
+                <h3><a href="../">Zur&uuml;ck</a></h3>
+            <br />
+        </div>
+    </body>
+</html>
+
+ 
+
+ 
+
+ 
+
+
