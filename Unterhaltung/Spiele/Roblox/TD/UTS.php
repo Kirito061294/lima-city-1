751b3d8a7dbeddd972a3dd301678dd95afb72efe
Kirito061294 <login@mwiese.de>
2022-09-27 18:10:08 +0200

    Update UTS.php

--- Unterhaltung/Spiele/Roblox/TD/UTS.php
+++ Unterhaltung/Spiele/Roblox/TD/UTS.php
@@ -5,11 +5,7 @@
 							$selected = $_POST['uts'];
 						}
 						echo '<option value="" disabled selected></option>';
-							add_option($selected,'uts00c','Client Mods');
-							add_option($selected,'uts00a','Launcher');
-							add_option($selected,'uts00b','Server Plugins');
-							//add_option($selected,'uts00d','Launcher');
-							//add_option($selected,'uts00e','Launcher');
+							add_option($selected,'uts00a','Türme');
 						?>
 
 						</select>
@@ -17,10 +13,6 @@
 					if (isset($_POST['uts'])) {
 						switch ($_POST['uts']) {
 							case 'uts00a': include ("PC/MC/L.php"); break;
-							case 'uts00b': include ("PC/MC/SP.php"); break;
-							case 'uts00c': include ("PC/MC/CM.php"); break;
-							//case 'uts00d': include ("PC/MC/L.php"); break;
-							//case 'uts00e': include ("PC/MC/L.php"); break;
 						}
 					}
 				?>
