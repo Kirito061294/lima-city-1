0783fd7f1d6a7edef124708913e39eab09243940
Kirito061294 <login@mwiese.de>
2022-09-27 17:23:30 +0200

    Update head1.php

--- include/head1.php
+++ include/head1.php
@@ -1,46 +1,37 @@
-<link rel="shortcut icon" href="https://mwiese.de/include/45454.ico" type="image/x-icon"/>
-        <link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style.css">
-        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+<link rel="shortcut icon" href="http://raspi.local:5000/include/45454.ico" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="http://raspi.local:5000/include/style.css">
+        <meta lang="de" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     </head>
 	<body>
         <header>
             <section>
                 <nav>
-				    <a class="logo" href="https://mwiese.de/Technik/test.php">mwiese.de</a>
+				    <a class="logo" href="raspi.local:5000">Home</a>
 				    <input type="checkbox" id="toggle_button">
-					<label for="toggle_button" class="toggle_button">
-					    <span class="bar"></span>
-					    <span class="bar"></span>
-					    <span class="bar"></span>
-					</label>
+					<label for="toggle_button" class="toggle_button"><span class="bar"></span><span class="bar"></span><span class="bar"></span></label>
                     <ul class="kopf">
                         <li class="expandable_li">
 						    <input type="checkbox" id="unterhaltung_checkbox">
 							<label for="unterhaltung_checkbox">Unterhaltung</label>
-						    <ul class="dropdown">
-								<li><a href="https://mwiese.de/Musik/M.php">Musik</a></li>
-								<li><a href="https://mwiese.de/Unterhaltung/Spiele/Spiele.php">Spiele</a></li>
-							</ul>
+						    <ul class="dropdown"><li><a href="http://raspi.local:5000/Unterhaltung/Musik/Musik.php">Musik</a></li><li><a href="http://raspi.local:5000/Unterhaltung/Spiele/Spiele.php">Spiele</a></li></ul>
 						</li>
                         <li class="expandable_li">
 						    <input type="checkbox" id="technik_checkbox">
 							<label for="technik_checkbox">Technik</label>
-						    <ul class="dropdown">
-								<li><a href="https://mwiese.de/Technik/OS/OS.php">Betriebssysteme</a></li>
-								<li><a href="https://mwiese.de/Technik/Netzwerk/Netzwerk.php">Internet/Netzwerk</a></li>
-								<li><a href="https://mwiese.de/Technik/Interessantes.php">Interessantes</a></li>
-							</ul>
+						    <ul class="dropdown"><li><a href="http://raspi.local:5000/Technik/OS/OS.php">Betriebssysteme</a></li><li><a href="http://raspi.local:5000/Technik/Netzwerk/Netzwerk.php">Internet/Netzwerk</a></li><li><a href="http://raspi.local:5000/Technik/Interessantes.php">Interessantes</a></li></ul>
+						</li>
+                        <li class="expandable_li">
+						    <input type="checkbox" id="spezielles_checkbox">
+							<label for="spezielles_checkbox">Spezielles</label>
+						    <ul class="dropdown"><li><a href="http://raspi.local:5000/spezielles/aussagen/aussagen.php">Aussagen</a></li></ul>
 						</li>
                         <li class="expandable_li">
 						    <input type="checkbox" id="subdomains_checkbox">
 							<label for="subdomains_checkbox">Unterseiten</label>
-						    <ul class="dropdown">
-								<li><a href="https://pc.mwiese.de" target="_blank">Mein Hobby</a></li>
-								<li><a href="https://mwiese.de/Themen/Themen.php">Spezielle Themen</a></li>
-							</ul>
+						    <ul class="dropdown"><li><a href="http://raspi.local:5000/Glaube/Glaube.php" target="_blank">Glaubensthemen</a></li><li><a href="https://pc.mwiese.de" target="_blank">Mein Hobby</a></li><li><a href="http://raspi.local:5000/Themen/Themen.php" target="_blank">Spezielle Themen</a></li></ul>
 						</li>
-                        <li><a href="https://mwiese.de/Rechtliches/Impressum.php">Impressum</a></li>
-                        <li><a href="https://mwiese.de/Rechtliches/Datenchutz.php">Datenschutz</a></li>
+                        <li><a href="http://raspi.local:5000/Rechtliches/Impressum.php" target="_blank">Impressum</a></li>
+                        <li><a href="http://raspi.local:5000/Rechtliches/Datenschutz.php" target="_blank">Datenschutz</a></li>
                     </ul>
                 </nav>
             </section>
