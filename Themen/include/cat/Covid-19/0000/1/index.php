92cef0519356830d6b8036058417850150050aeb
Kirito061294 <login@mwiese.de>
2022-09-27 17:27:38 +0200

    Create index.php

---
+++ Themen/include/cat/Covid-19/0000/1/index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <title>Covid-19</title>
+        <?php include ("../../../include/head.php"); ?>
+    </head>
+    <body>
+	    <?php include ("../../../include/header.php"); ?>
+	    <div align="center">
+                <h1>Wilkommen!</h1>
+            <br />
+            <br />
+			    <h2><a href="german.php">Deutsche &Uuml;bersetzung</a><br /><br />
+				Die Quelle dieser Texte ist <a href="https://bit.ly/3EzFpu1">hier</a>!</h2>
+            <br />
+                <h1>Abortion opponents protest COVID-19 vaccines' use of fetal cells</h1><br />
+				<h2>Two candidates on White House short list will receive up to $1.7 billion<br /><br />
+				Science's COVID-19 reporting is supported by the Pulitzer Center.</h2><br /><br />
+				<h3>
+				<font color="black">
+					Senior Catholic leaders in the United States and Canada, <br />
+					along with other antiabortion groups, are raising ethical objections to promising COVID-19 vaccine candidates <br />
+					that are manufactured using cells derived from human fetuses electively aborted decades ago. <br />
+					They have not sought to block government funding for the vaccines, <br />
+					which include two candidate vaccines that the Trump administration plans to support with an investment of up to $1.7 billion, <br />
+					as well as a third candidate made by a Chinese company in collaboration with Canada's National Research Council (NRC). <br />
+					But they are urging funders and policymakers to ensure that companies develop other <br />
+					vaccines that do not rely on such human fetal cell lines and, in the United States, <br />
+					asking the government to "incentivize" firms to only make vaccines that don't rely on fetal cells.<br /><br />
+					
+					"It is critically important that Americans have access to a vaccine that is produced ethically: <br />
+					no American should be forced to choose between being vaccinated against this <br />
+					potentially deadly virus and violating his or her conscience,"<br />
+					members of the U.S. Conference of Catholic Bishops and 20 other religious,<br />
+					medical, and political organizations that oppose abortion <a href="https://bit.ly/3mArrSy">wrote to</a> Stephen Hahn, <br />
+					commissioner of the U.S. Food and Drug Administration (FDA), in April. "Thankfully, <br />
+					other [COVID-19] vaccines … utilize cell lines not connected to unethical procedures and methods."<br /><br />
+					
+					"We urge your government to fund the development of vaccines that do not create an ethical dilemma for many Canadians," <br />
+					wrote Archbishop of Winnipeg Richard Gagnon, president of the Canadian Conference of Catholic Bishops, <br />
+					and 17 other antiabortion religious, medical, <br />
+					and politic groups and individuals in a <a href="https://bit.ly/3nRytC1">21 May letter</a> to Prime Minister Justin Trudeau. <br />
+					"The … manufacture of vaccines using such ethically-tainted human cell lines <br />
+					demonstrates profound disrespect for the dignity of the human person."<br /><br />
+					
+					FDA and senior White House officials did not respond to emails requesting comment on the letter to Hahn. <br />
+					In Canada, the health ministry has promised to respond to the letter to Trudeau, says Moira McQueen, <br />
+					executive director of the Canadian Catholic Bioethics Institute and lead signatory on the letter.<br /><br />
+					
+					Cells derived from elective abortions have been used since the 1960s to manufacture vaccines, <br />
+					including current vaccines against rubella, chickenpox, hepatitis A, and shingles. <br />
+					They have also been used to make approved drugs against diseases including hemophilia, rheumatoid arthritis, and cystic fibrosis. <br />
+					Now, research groups around the world are working to develop more than 130 candidate vaccines against COVID-19, <br />
+					according to the World Health Organization; 10 had entered human trials as of 2 June.<br /><br />
+					
+					At least five of the candidate COVID-19 vaccines use one of two human fetal cell lines: HEK-293,<br />
+					a kidney cell line widely used in research and industry that comes from a fetus aborted in about 1972; and PER.C6, <br />
+					a proprietary cell line owned by Janssen, a subsidiary of Johnson & Johnson, developed from retinal cells from an 18-week-old fetus aborted in 1985. <br />
+					Both cell lines were developed in the lab of molecular biologist Alex van der Eb at Leiden University. <br />
+					Two of the five vaccines have entered human trials<br /><br />
+					
+					In four of the vaccines, the human fetal cells are used as miniature "factories" to generate vast quantities of adenoviruses, <br />
+					disabled so that they cannot replicate, that are used as vehicles to ferry genes from the novel coronavirus that causes COVID-19.<br />
+					When the adenoviruses are given as a vaccine, <br />
+					recipients' cells begin to produce proteins from the coronavirus, hopefully triggering a protective immune response.<br /><br />
+					
+					The fifth vaccine, which has shown promise in monkeys and is headed for human trials as soon as this summer, <br />
+					is what is known as a protein subunit vaccine. <br />
+					Researchers at the University of Pittsburgh use HEK-293 cells to manufacture the coronavirus' <br />
+					spike protein—a vital part of its structure—which is used to trigger an immune response. <br />
+					The vaccine is delivered through a skin patch with 400 tiny needles.<br /><br />
+					
+					The fetal cell lines are key to producing both types of vaccine. <br />
+					"HEK-293 [cells] are essential for making protein subunit vaccines," says Andrea Gambotto, <br />
+					a vaccine scientist at the University of Pittsburgh School of Medicine and the vaccine's lead developer. <br />
+					Their human origin is important, he says: "Cultured [nonhuman] animal cells can produce the same proteins, <br />
+					but they would be decorated with different sugar molecules, <br />
+					which—in the case of vaccines—runs the risk of failing to evoke a robust and specific immune response." <br />
+					(Among the developers of the five vaccines, only Gambotto responded to a request for comment.)<br /><br />
+					
+					David Prentice, vice president and research director at the Charlotte Lozier Institute, which opposes abortion, <br />
+					notes researchers making adenovirus vaccines have modified HEK-293 cells to be adept at packaging new genes—such <br />
+					as those that direct cells to assemble the coronavirus spike protein—into adenoviruses. <br />
+					But he adds that other technologies are available, <br />
+					including using cells captured from amniocentesis that are engineered to make replication-deficient adenoviruses.<br /><br />
+					
+					"The use of cells from electively aborted fetuses for vaccine production makes these five COVID-19 vaccine programs unethical, <br />
+					because they exploit the innocent human beings who were aborted," <br />
+					Prentice and a co-author—molecular biologist James Sherley, <br />
+					a Lozier Institute associate scholar and director of the adult stem cell company Asymmetrex—wrote <a href="https://bit.ly/3mxBQOJ">in a position paper</a> published last month.<br /><br />
+					
+					But Arthur Caplan, a bioethicist at the New York University School of Medicine, counters: <br />
+					"There are better ways to win the abortion wars than telling people not to use a vaccine. These are long-over abortions. <br />
+					These cells are decades old, and even major religious leaders like the pope have acknowledged <br />
+					that for the greater good it's not worth the symbolism to put the community at risk."<br /><br />
+					
+					The Vatican's Pontifical Academy for Life <a href="https://bit.ly/3w7iAuP">declared in 2005</a> and <a href="https://bit.ly/3w610XV">reaffirmed in 2017</a> that in the absence of alternatives, <br />
+					Catholics could, in good conscience, receive vaccines made using historical human fetal cell lines.<br /><br />
+					
+					A vaccine made by the Chinese company CanSino Biologics was the first COVID-19 vaccine to enter phase II human trials. <br />
+					It was developed using adapted HEK-293 cells that the company licensed from Canada's NRC, where the cells were developed. <br />
+					(NRC-developed HEK-293 cells have already been used to develop an approved Ebola vaccine.) <br />
+					Last month, NRC <a href="https://bit.ly/3mze0lN">announced a collaboration with CanSino Biologics</a> under which it is preparing to run <br />
+					late-stage clinical trials of the vaccine in Canada, and scale up facilities to produce the vaccine in quantity.<br /><br />
+					
+					The two U.S.-backed vaccines that have drawn criticism from antiabortion groups are <a href="https://bit.ly/3pUQqlw">on a short list of candidates</a> <br />
+					targeted to get financial and logistical support from the U.S. government under the White House's Operation Warp Speed, <br />
+					which aims to accelerate the development and approval of at least one COVID-19 vaccine by January 2021, <br />
+					according to <a href="https://nyti.ms/3bxpfFe">a 3 June report in The New York Times</a>.<br /><br />
+					
+					One of the Warp Speed candidates, made by Janssen Research & Development, uses PER.C6 cells.<br />
+					The second, from University of Oxford researchers and AstraZeneca, <br />
+					uses HEK-293 cells. Both have received U.S. government commitments of, respectively, $456 million and $1.2 billion, <br />
+					if they meet milestones, through the Biomedical Advanced Research Development Authority (BARDA).<br /><br />
+					
+					Another vaccine that relies on HEK-293, <a href="https://bit.ly/3GFfjaU">being developed by two companies</a> owned by the billionaire scientist <br />
+					and businessman Patrick Soon-Shiong, made an earlier, Warp Speed long list of 14 promising candidates, <br />
+					according to a <a href="https://bit.ly/3pTdUHX">press release</a> from one of companies, NantKwest.<br /><br />
+					
+					Prentice says: "As they are choosing—BARDA and the Warp Speed people— what vaccines to move ahead, <br />
+					they should at least recognize that there is some portion of the population who would like an alternative vaccine they can take in good conscience."<br /><br />
+					
+					Caplan disagrees. "If you are going to say the government shouldn't fund things that a minority of people object to, <br />
+					you will have a very long list of things that won't get funded by the government, from research on weapons of war to contraceptive research."<br /><br />
+					
+					The Trump administration has restricted the use of human fetal tissue from elective abortions in biomedical research. <br />
+					One year ago, it adopted a policy that forbids researchers at the <br />
+					National Institutes of Health (NIH) from using fetal tissue from elective abortions in their studies. <br />
+					And it imposed an extra layer of review on non-NIH scientists seeking agency funding to do research using such tissue. <br />
+					But the policy did not stop either group from using decades-old fetal cell lines like HEK-293 and PER.C6.<br /><br />
+					
+					*Clarification, 8 June, 12:10 p.m.: <br />
+					This story has been updated to clarify that the Vatican approves of Catholics <br />
+					receiving vaccines manufactured using human fetal cells only in the absence of alternatives.<br />
+				</font>
+				</h3>
+            <br />
+            <br />
+                <h3><a href="../">Zur&uuml;ck</a></h3>
+            <br />
+        </div>
+    </body>
+</html>
+
+ 
+
+
+
+
