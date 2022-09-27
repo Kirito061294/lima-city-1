0f7dd49473af52da490c3e364593eb7dace8060a
Kirito061294 <login@mwiese.de>
2022-09-27 17:26:26 +0200

    Create ME_Tricks.php

---
+++ Technik/OS/MS/ME/ME_Tricks.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+      "http://www.w3.org/TR/html4/loose.dtd">
+
+
+<html>
+    <head>
+        <title>Windows ME Tricks</title>
+        <?php include ("../../../../include/head1.php"); ?>
+            <div class="ueberschrift">
+                <h1>Windows ME Tricks</h1>
+			</div>
+            <div class="linkzone01">
+                <p><a href="Tricks/akti.php">WinME Aktivierung</a></p>
+            </div>
+                <?php include ("../../../../include/footer1.php"); ?>
+</html>
