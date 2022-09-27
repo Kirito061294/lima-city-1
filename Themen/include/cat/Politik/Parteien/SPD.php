a4c2e5eaaf3083612c665c7147df6e2417576bda
Kirito061294 <login@mwiese.de>
2022-09-27 17:29:22 +0200

    Create SPD.php

---
+++ Themen/include/cat/Politik/Parteien/SPD.php
@@ -0,0 +1,38 @@
+<p><a href="M-Schwesig">Manuela Schwesig</a><a href="https://de.wikipedia.org/wiki/Manuela_Schwesig"><font color="black"> - Wikipedia</font></a><br />
+				    <a href="P-Tschetschener">Peter Tschetschener</a><a href="https://de.wikipedia.org/wiki/Peter_Tschentscher"><font color="black"> - Wikipedia</font></a><br />
+				    <a href="S-Weil">Stephan Weil</a><a href="https://de.wikipedia.org/wiki/Stephan_Weil"><font color="black"> - Wikipedia</font></a><br />
+				    <a href="D-Woidke">Dietmar Woidke</a><a href="https://de.wikipedia.org/wiki/Dietmar_Woidke"><font color="black"> - Wikipedia</font></a></p>
+				    <p><a href="S-Esken">Saskia Esken</a><a href="https://de.wikipedia.org/wiki/Saskia_Esken"><font color="black"> - Wikipedia</font></a><br />
+				    <a href="N-Walter-Borjans">Norbert Walter-Borjans</a><a href="https://de.wikipedia.org/wiki/Norbert_Walter-Borjans"><font color="black"> - Wikipedia</font></a><br />
+				    <a href="K-Geywitz">Klara Geywitz</a><a href="https://de.wikipedia.org/wiki/Klara_Geywitz"><font color="black"> - Wikipedia</font></a><br />
+				    <a href="H-Heil">Hubertus Heil</a><a href="https://de.wikipedia.org/wiki/Hubertus_Heil"><font color="black"> - Wikipedia</font></a><br />
+				    <a href="K-Kühnert">Kevin Kühnert</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="S-Midyatli">Serpil Midyatli</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="A-Rehlinger">Anke Rehlinger</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="L-Klingbeil">Lars Klingbeil</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="D-Nietan">Dietmar Nietan</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="U-Bullmann">Udo Bullmann</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="D-Ahnen">Doris Ahnen</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="K-Pähle">Katja Pähle</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="J-Wischmeier">Jessika Wischmeier</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="L-Breymaier">Leni Breymaier</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="M-Dulig">Martin Dulig</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="M-Engelmeier">Michaela Engelmeier</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="W-Esdar">Wiebke Esdar</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="F-Giffey">Franziska Giffey</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="K-Griese">Kerstin Griese</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="U-Grötsch">Uli Grötsch</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="G-A.Horn">Gustav A. Horn</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="O-Kaczmarek">Oliver Kaczmarek</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="H-Maas">Heiko Maas</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="B-Martin">Bettina Martin</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="M-Miersch">Matthias Miersch</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="A-Özoğuz">Aydan Özoğuz</a><a href="https://de.wikipedia.org/wiki/Aydan_%C3%96zo%C4%9Fuz"><font color="black"> - Wikipedia</font></a><br />
+				    <a href="B-Pistorius">Boris Pistorius</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="M-Roth">Michael Roth</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="S-Ryglewski">Sarah Ryglewski</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="D-Schmidt">Dagmar Schmidt</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="S-Schulze">Svenja Schulze</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="A-Schweitzer">Alexander Schweitzer</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="A-Stoch">Andreas Stoch</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="J-Uekermann">Johanna Uekermann</a><a href=""><font color="black"> - Wikipedia</font></a></p>
