55284353f38925c96506b0367e7361967beb00e2
Kirito061294 <login@mwiese.de>
2022-09-27 17:23:15 +0200

    Create wkidh.php

---
+++ Glaube/themen/wkidh.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "https://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <title>mwiese.de</title>
+        <?php include ("../include/head.php"); ?>
+    </head>
+    <body>
+	    <?php include ("../include/header.php"); ?>
+	    <div align="center">
+                <h1>Wilkommen :)</h1>
+            <br />
+            <br />
+                <h3>
+				Zu diesem Thema gibt es leider viel zu viele falsche Meinungen und Ansätze...<br /><br />
+				<font color="black">Punkt 1: Niemand kommt in die Hölle, ohne sie zu Verdienen.<br /><br />
+				Punkt 2: ALLE Menschen verdienen die Hölle, denn sie sind schuldig weil Adam schuldig ist.<br /><br />
+				Punkt 3: ALLE Menschen die Jesus als ihren Herrn und Retter angenommen haben<br />sind - ohne wenn und aber - gerettet, von aller Schuld befreit und werden für immer bei IHM sein.</font><br /><br />
+				Ich höre/lese viel zu oft sowas wie "Du musst dies und jenes tun sonst ist Gott böse auf dich".<br />
+				<font color="black">Punkt 4: Gott liebt ALLE Menschen exakt gleich, ungeachtet ihrer Herkunft, Spiritualität, Hautfarbe...</font><br />
+				Sollten Sie von irgendjemandem also gesagt bekommen das Gott Sie nicht liebt wenn sie was auch immer tun... siehe Punkt 4 :)<br /><br />
+				<br />
+				<br /><br />
+				<br /><br />
+				</h3>
+            <br /><br /><br />
+                <h3><a href="../">Zur&uuml;ck</a></h3>
+            <br />
+        </div>
+    </body>
+</html>
+
+<a href=""></a>
