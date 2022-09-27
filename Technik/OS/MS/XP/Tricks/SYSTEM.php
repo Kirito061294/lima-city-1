42f39b68595e521d735d56d8a32b32642b9e0699
Kirito061294 <login@mwiese.de>
2022-09-27 17:26:43 +0200

    Create SYSTEM.php

---
+++ Technik/OS/MS/XP/Tricks/SYSTEM.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+      "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <title>mwiese.de</title>
+        <?php include ("../../../../../include/head1.php"); ?>
+            <div class="ueberschrift">
+                <h1>SYSTEM-Account Zugang</h1>
+			</div>
+            <div class="linkzone01">
+                <p></p>
+            </div>
+                <?php include ("../../../../../include/footer1.php"); ?>
+</html>
