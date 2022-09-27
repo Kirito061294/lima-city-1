0756d96ce58402fd5c1272fcc0f3796e28760c19
Kirito061294 <login@mwiese.de>
2022-09-27 17:29:06 +0200

    Create B90.php

---
+++ Themen/include/cat/Politik/Parteien/B90.php
@@ -0,0 +1,75 @@
+<p><a href="">Annalena Baerbock</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Robert Habeck</a> - <a href=""><font color="black">Wikipedia</font></a></p>
+					
+				    <p><a href="">Michael Kellner</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Jamila Schaefer</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Ricarda Lang</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Marc Urbatsch</a> - <a href=""><font color="black">Wikipedia</font></a></p>
+					
+					<p><font color="green">Der Wirtschaftsbeirat:</font></p>
+					
+				    <p><a href="">Katharina Beck</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Sven Becker</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Lukas Beckmann</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Emilie Bourgoin</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Martin Brudermüller</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Rolf Brunkhorst</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Reinhard Bütikofer</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Rolf Bulander</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Gerd Chrzanowski</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Premal A. Desai</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Matthias Dümpelmann</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Andrea Sibylle Ebinger</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Sebastian Frevel</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Thomas Gambke</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Timothy Glaz</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Jacob Götze</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Ana Christina Grohnert</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Gottfried Härle</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Alexander Hock</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Frank Holtrup</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Dietmar Huber</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Thomas Jorberg</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Sonja Jost</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Tom Kirschbaum</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Patrick Kopischke</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Henning Krumrey</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Arndt Kwiatkowski</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Frank Leschhorn</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Bernhard Lorentz</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Sven Lundie</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Andreas Mattner</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Wolfgang Mennicken</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Johannes Merck</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Klaus Müller</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Sabine Nallinger</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Henriette Peucker</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Hagen Pfundner</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Ramona Pop</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Andreas Rade</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Carlos Rasel</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Andreas Renner</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Katharina Reuter</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Moritz Ritter</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Amir Roughani</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Sandra Schneeloch</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Solveig Schulze</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">André Schwämmlein</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Daniel Sehrt</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Ole Seidel</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Bert Sutter</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Robin P. G. Tech</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Claudia Tober</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Torsten Wunderlich</a><a href=""><font color="black"> - Wikipedia</font></a><br />
+				    <a href="">Christoph Zschocke</a><a href=""><font color="black"> - Wikipedia</font></a></p>
+					
+					<p><font color="green">Die grüne Bundestagsfraktion vertreten im Wirtschaftsbeirat:</font></p>
+					
+				    <p><a href="">Danyal Bayaz</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Annalena Baerbock</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Katharina Dröge</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Katrin Göring-Eckardt</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Anja Hajduk</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Dieter Janecek</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Oliver Krischer</a> - <a href=""><font color="black">Wikipedia</font></a><br />
+				    <a href="">Claudia Müller</a> - <a href=""><font color="black">Wikipedia</font></a></p>
