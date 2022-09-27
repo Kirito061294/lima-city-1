64147b5496daa5c5eec32b8e0ac065e784e53e3f
Kirito061294 <login@mwiese.de>
2022-09-27 17:27:28 +0200

    Create german.php

---
+++ Themen/include/cat/Covid-19/0000/1/german.php
@@ -0,0 +1,154 @@
+<html>
+    <head>
+        <title>Covid-19</title>
+        <?php include ("../../../include/head.php"); ?>
+    </head>
+    <body>
+	    <div align="center">
+            <br />
+			    <h2>
+		            Dieser Text wurde mit DeepL übersetzt.
+				</h2>
+            <br />
+				<h3>
+				<font color="black">
+				<h2>Die Quelle dieser Texte ist <a href="https://bit.ly/3EzFpu1">hier</a>!</h2><br /><br />
+				
+				<h1>Abtreibungsgegner protestieren gegen die Verwendung von fötalen Zellen in COVID-19-Impfstoffen</h1><br /><br />
+				<h2>Zwei Kandidaten auf der Auswahlliste des Weißen Hauses erhalten bis zu 1,7 Milliarden Dollar<br /><br />
+				Die COVID-19-Berichterstattung von Science wird vom Pulitzer Center unterstützt.</h2><br /><br />
+                <br />
+				Führende katholische Politiker in den Vereinigten Staaten und Kanada erheben zusammen mit anderen Abtreibungsgegnern <br />
+				ethische Einwände gegen vielversprechende COVID-19-Impfstoffkandidaten, die unter Verwendung von Zellen hergestellt werden, <br />
+				die von menschlichen Föten stammen, die vor Jahrzehnten freiwillig abgetrieben wurden. Sie haben nicht versucht, <br />
+				die staatliche Finanzierung der Impfstoffe zu blockieren, zu denen zwei Impfstoffkandidaten gehören, <br />
+				die die Trump-Administration mit einer Investition von bis zu 1,7 Milliarden Dollar unterstützen will, <br />
+				sowie ein dritter Kandidat, der von einem chinesischen Unternehmen in Zusammenarbeit mit dem <br />
+				kanadischen National Research Council (NRC) entwickelt wurde. Sie fordern jedoch Geldgeber und politische Entscheidungsträger auf, <br />
+				dafür zu sorgen, dass Unternehmen andere Impfstoffe entwickeln, die nicht auf solche menschlichen fötalen Zelllinien angewiesen sind,<br />
+				und in den Vereinigten Staaten die Regierung aufzufordern, "Anreize" für Unternehmen zu schaffen, <br />
+				nur Impfstoffe herzustellen, die nicht auf fötalen Zellen beruhen.<br />
+				<br />
+				"Es ist von entscheidender Bedeutung, dass die Amerikaner Zugang zu einem Impfstoff haben, der unter ethischen Gesichtspunkten hergestellt wird: <br />
+				Kein Amerikaner sollte gezwungen sein, zwischen einer Impfung gegen dieses potenziell tödliche Virus und der Verletzung seines oder ihres Gewissens zu wählen", <br />
+				schrieben Mitglieder der US-Konferenz der katholischen Bischöfe und 20 weitere religiöse, medizinische und politische Organisationen, <br />
+				die sich gegen die Abtreibung aussprechen, im April an Stephen Hahn, den Kommissar der US-amerikanischen Food and Drug Administration (FDA). <br />
+				"Glücklicherweise verwenden andere [COVID-19]-Impfstoffe ... Zelllinien, die nicht mit unethischen Verfahren und Methoden verbunden sind."<br />
+				<br />
+				"Wir fordern Ihre Regierung auf, die Entwicklung von Impfstoffen zu finanzieren, die viele Kanadier nicht in ein ethisches Dilemma bringen", <br />
+				schrieb der Erzbischof von Winnipeg, Richard Gagnon, Präsident der Kanadischen Katholischen Bischofskonferenz, zusammen mit 17 anderen religiösen,<br />
+				medizinischen und politischen Gruppen und Einzelpersonen, die sich gegen Abtreibung einsetzen, <br />
+				in einem Brief an Premierminister Justin Trudeau vom 21. Mai.<br />
+				"Die ... Herstellung von Impfstoffen unter Verwendung solcher ethisch verunreinigter menschlicher Zelllinien <br />
+				zeigt eine tiefe Missachtung der Würde der menschlichen Person."<br />
+				<br />
+				Die FDA und hochrangige Beamte des Weißen Hauses reagierten nicht auf E-Mails, in denen sie um eine Stellungnahme zu dem Brief an Hahn gebeten wurden. <br />
+				In Kanada hat das Gesundheitsministerium versprochen, auf den Brief an Trudeau zu antworten, sagt Moira McQueen, <br />
+				Geschäftsführerin des Canadian Catholic Bioethics Institute und Erstunterzeichnerin des Briefes.<br />
+				<br />
+				Zellen aus freiwilligen Schwangerschaftsabbrüchen werden seit den 1960er Jahren zur Herstellung von Impfstoffen verwendet, <br />
+				darunter aktuelle Impfstoffe gegen Röteln, Windpocken, Hepatitis A und Gürtelrose. <br />
+				Sie wurden auch zur Herstellung von zugelassenen Medikamenten gegen Krankheiten wie Hämophilie, rheumatoide Arthritis und Mukoviszidose verwendet. <br />
+				Nach Angaben der Weltgesundheitsorganisation arbeiten Forschergruppen in aller Welt an der Entwicklung von mehr als 130 Impfstoffkandidaten gegen COVID-19;<br />
+				bis zum 2. Juni hatten 10 von ihnen Versuche am Menschen begonnen.<br />
+				<br />
+				Mindestens fünf der COVID-19-Impfstoffkandidaten verwenden eine von zwei menschlichen fötalen Zelllinien: HEK-293, <br />
+				eine in Forschung und Industrie weit verbreitete Nierenzelllinie, die von einem etwa 1972 abgetriebenen Fötus stammt, <br />
+				und PER.C6, eine geschützte Zelllinie im Besitz von Janssen, einer Tochtergesellschaft von Johnson & Johnson, <br />
+				die aus Netzhautzellen eines 1985 abgetriebenen 18 Wochen alten Fötus entwickelt wurde. <br />
+				Beide Zelllinien wurden im Labor des Molekularbiologen Alex van der Eb an der Universität Leiden entwickelt. <br />
+				Für zwei der fünf Impfstoffe wurden bereits Versuche am Menschen durchgeführt.<br />
+				<br />
+				In vier der Impfstoffe werden die menschlichen fötalen Zellen als Miniatur-"Fabriken" verwendet, <br />
+				um große Mengen von Adenoviren zu erzeugen, die so deaktiviert sind, dass sie sich nicht vermehren können, <br />
+				und die als Vehikel für den Transport von Genen des neuartigen Coronavirus dienen, das COVID-19 verursacht. <br />
+				Wenn die Adenoviren als Impfstoff verabreicht werden, beginnen die Zellen des Empfängers, <br />
+				Proteine des Coronavirus zu produzieren, was hoffentlich eine schützende Immunreaktion auslöst.<br />
+				<br />
+				Der fünfte Impfstoff, der sich bei Affen als vielversprechend erwiesen hat und noch in diesem Sommer für Versuche am Menschen vorgesehen ist, <br />
+				ist ein so genannter Protein-Untereinheiten-Impfstoff. <br />
+				Forscher an der Universität Pittsburgh verwenden HEK-293-Zellen zur Herstellung des Spike-Proteins des Coronavirus - <br />
+				einem wichtigen Teil seiner Struktur -, das zur Auslösung einer Immunreaktion verwendet wird. <br />
+				Der Impfstoff wird über ein Hautpflaster mit 400 winzigen Nadeln verabreicht.<br />
+				<br />
+				Die fötalen Zelllinien sind der Schlüssel zur Herstellung beider Arten von Impfstoffen. <br />
+				"HEK-293 [Zellen] sind für die Herstellung von Proteinuntereinheiten-Impfstoffen unerlässlich", sagt Andrea Gambotto, <br />
+				Impfstoffwissenschaftler an der University of Pittsburgh School of Medicine und leitender Entwickler des Impfstoffs. <br />
+				Ihre menschliche Herkunft ist wichtig, sagt er: "Gezüchtete [nichtmenschliche] Tierzellen können die gleichen Proteine produzieren, <br />
+				aber sie würden mit anderen Zuckermolekülen dekoriert werden, was im Falle von Impfstoffen das Risiko birgt, <br />
+				keine robuste und spezifische Immunantwort hervorzurufen." <br />
+				(Von den Entwicklern der fünf Impfstoffe reagierte nur Gambotto auf eine Anfrage zur Stellungnahme).<br />
+				<br />
+				David Prentice, Vizepräsident und Forschungsdirektor des Charlotte Lozier Institute, das sich gegen Abtreibung einsetzt, stellt fest, <br />
+				dass Forscher, die Adenovirus-Impfstoffe herstellen, HEK-293-Zellen so modifiziert haben, dass sie neue Gene in Adenoviren verpacken können, <br />
+				z. B. solche, die die Zellen anweisen, das Coronavirus-Spike-Protein zusammenzubauen. <br />
+				Aber er fügt hinzu, dass auch andere Technologien zur Verfügung stehen, <br />
+				einschließlich der Verwendung von Zellen,<br />
+				die bei der Fruchtwasseruntersuchung entnommen und so verändert wurden, dass sie Adenoviren mit Replikationsdefiziten bilden.<br />
+				<br />
+				"Die Verwendung von Zellen aus abgetriebenen Föten für die Impfstoffproduktion macht diese fünf COVID-19-Impfprogramme unethisch,<br />
+				da sie die unschuldigen menschlichen Wesen, die abgetrieben wurden, ausbeuten", <br />
+				schrieben Prentice und ein Mitautor - der Molekularbiologe James Sherley, <br />
+				ein assoziierter Wissenschaftler des Lozier-Instituts und Direktor des Unternehmens für adulte Stammzellen Asymmetrex - <br />
+				in einem im letzten Monat veröffentlichten Positionspapier.<br />
+				<br />
+				Doch Arthur Caplan, Bioethiker an der New York University School of Medicine, hält dagegen: <br />
+				"Es gibt bessere Wege, die Abtreibungskriege zu gewinnen, als den Leuten zu sagen, dass sie einen Impfstoff nicht verwenden sollen. <br />
+				Es handelt sich um längst abgeschlossene Abtreibungen. <br />
+				Diese Zellen sind Jahrzehnte alt, und selbst wichtige religiöse Führer wie der Papst haben anerkannt, <br />
+				dass es für das Allgemeinwohl nicht die Symbolik wert ist, die Gemeinschaft zu gefährden."<br />
+				<br />
+				Die Päpstliche Akademie für das Leben des Vatikans erklärte 2005 und bekräftigte 2017, <br />
+				dass Katholiken in Ermangelung von Alternativen mit gutem Gewissen Impfstoffe erhalten können, <br />
+				die unter Verwendung historischer menschlicher fötaler Zelllinien hergestellt wurden.<br />
+				<br />
+				Ein von dem chinesischen Unternehmen CanSino Biologics hergestellter Impfstoff war der erste COVID-19-Impfstoff, <br />
+				der in Phase II beim Menschen getestet wurde. Er wurde mit angepassten HEK-293-Zellen entwickelt, <br />
+				die das Unternehmen vom kanadischen NRC lizenziert hat, wo die Zellen entwickelt wurden. <br />
+				(Vom NRC entwickelte HEK-293-Zellen wurden bereits für die Entwicklung eines zugelassenen Ebola-Impfstoffs verwendet). <br />
+				Letzten Monat gab NRC eine Zusammenarbeit mit CanSino Biologics bekannt, <br />
+				in deren Rahmen das Unternehmen die Durchführung von klinischen Versuchen im Spätstadium des Impfstoffs in Kanada <br />
+				und den Aufbau von Anlagen zur Herstellung des Impfstoffs in großen Mengen vorbereitet.<br />
+				<br />
+				Die beiden von den USA unterstützten Impfstoffe, die bei Abtreibungsgegnern auf Kritik gestoßen sind, <br />
+				stehen auf einer kurzen Liste von Kandidaten, die im Rahmen der Operation Warp Speed des Weißen Hauses <br />
+				finanzielle und logistische Unterstützung von der US-Regierung erhalten sollen. <br />
+				Laut einem Bericht der New York Times vom 3. Juni soll damit die Entwicklung und Zulassung <br />
+				mindestens eines COVID-19-Impfstoffs bis Januar 2021 beschleunigt werden.<br />
+				<br />
+				Einer der Warp-Speed-Kandidaten von Janssen Research & Development verwendet PER.C6-Zellen. <br />
+				Der zweite, der von Forschern der Universität Oxford und AstraZeneca entwickelt wurde, verwendet HEK-293-Zellen. <br />
+				Für beide hat die US-Regierung im Rahmen der Biomedical Advanced Research Development Authority (BARDA)<br />
+				456 Mio. $ bzw. 1,2 Mrd. $ zugesagt, sofern sie Meilensteine erreichen.<br />
+				<br />
+				Ein weiterer Impfstoff, der auf HEK-293 basiert und von zwei Unternehmen im Besitz des <br />
+				milliardenschweren Wissenschaftlers und Geschäftsmanns Patrick Soon-Shiong entwickelt wird, <br />
+				schaffte es laut einer Pressemitteilung von NantKwest, einem der Unternehmen, auf eine frühere Liste von 14 vielversprechenden Kandidaten.<br />
+				<br />
+				Prentice sagt: "Wenn sie - BARDA und die Warp-Speed-Leute - entscheiden, welche Impfstoffe sie einsetzen wollen, <br />
+				sollten sie zumindest anerkennen, dass es einen Teil der Bevölkerung gibt, <br />
+				der einen alternativen Impfstoff möchte, den er mit gutem Gewissen nehmen kann."<br />
+				<br />
+				Caplan ist da anderer Meinung. "Wenn Sie sagen, dass die Regierung keine Dinge finanzieren sollte,<br />
+				die eine Minderheit von Menschen ablehnt, werden Sie eine sehr lange Liste von Dingen haben, <br />
+				die nicht von der Regierung finanziert werden, von der Forschung über Kriegswaffen bis hin zur Verhütungsforschung.<br />
+				<br />
+				Die Trump-Regierung hat die Verwendung von menschlichem fötalem Gewebe aus freiwilligen Schwangerschaftsabbrüchen in der biomedizinischen Forschung eingeschränkt. <br />
+				Vor einem Jahr verabschiedete sie eine Richtlinie, die es Forschern an den National Institutes of Health (NIH) verbietet, <br />
+				fötales Gewebe von freiwilligen Abtreibungen in ihren Studien zu verwenden. Außerdem wurden Wissenschaftler, <br />
+				die keine NIH-Mitarbeiter sind und die Mittel der Behörde für Forschungsarbeiten mit solchem Gewebe beantragen, 
+				einer zusätzlichen Prüfung unterzogen. <br />
+				Die Richtlinie hinderte die beiden Gruppen jedoch nicht daran, jahrzehntealte fötale Zelllinien wie HEK-293 und PER.C6 zu verwenden.<br />
+				<br />
+				*Klarstellung, 8. Juni, 12:10 Uhr:<br />
+				Dieser Artikel wurde aktualisiert, um klarzustellen, dass der Vatikan den Erhalt von Impfstoffen, <br />
+				die unter Verwendung menschlicher fötaler Zellen hergestellt wurden, für Katholiken nur in Ermangelung von Alternativen genehmigt.<br />
+				</font>
+				</h3>
+            <br />
+            <br />
+                <h3><a href="../">Zur&uuml;ck</a></h3>
+            <br />
+        </div>
+    </body>
+</html>
