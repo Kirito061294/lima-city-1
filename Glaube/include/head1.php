4aa8529aaac3a5e7cafdc493c08dc9fb87197dbb
Kirito061294 <login@mwiese.de>
2022-09-27 17:22:50 +0200

    Create head1.php

---
+++ Glaube/include/head1.php
@@ -0,0 +1,24 @@
+<link rel="shortcut icon" href="http://raspi.local:5000/include/45454.ico" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="http://raspi.local:5000/include/style.css">
+        <meta lang="de" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    </head>
+	<body>
+        <header>
+            <section>
+                <nav>
+				    <a class="logo" href="raspi.local:5000">Home</a>
+				    <input type="checkbox" id="toggle_button">
+					<label for="toggle_button" class="toggle_button"><span class="bar"></span><span class="bar"></span><span class="bar"></span></label>
+                    <ul class="kopf">
+                        <li class="expandable_li">
+						    <input type="checkbox" id="technik_checkbox">
+							<label for="technik_checkbox">Themen</label>
+						    <ul class="dropdown"><li><a href="themen/judr.php">Jesus und die Religionen</a></li><li><a href="themen/gs-jn.php">Gott schauen - Ja/Nein?</a></li><li><a href="themen/wkidh.php">Wer kommt in die Hölle?</a></li></ul>
+						</li>
+                        <li><a href="http://raspi.local:5000/Rechtliches/Impressum.php" target="_blank">Impressum</a></li>
+                        <li><a href="http://raspi.local:5000/Rechtliches/Datenschutz.php" target="_blank">Datenschutz</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </header>
+        <div class="center1">
