4bc0ad083b8339b32c6f4163a8b1793cadb9e3bb
Kirito061294 <login@mwiese.de>
2022-10-06 19:44:29 +0200

    Update mythical.php

--- Unterhaltung/Spiele/Roblox/TD/UTS/Tower/mythical.php
+++ Unterhaltung/Spiele/Roblox/TD/UTS/Tower/mythical.php
@@ -1,18 +1,18 @@
-<select name="mythical">
+<select name="divine">
 									<?php
 						$selected = '';
 						if (isset($_POST['submit'])) {
-							$selected = $_POST['mythical'];
+							$selected = $_POST['divine'];
 						}
 						echo '<option value="" disabled selected></option>';
-							//add_option($selected,'mythical00a','');
+							add_option($selected,'divine00a','Blackbeard');
 						?>
 
 								</select>
 									<?php
-					if (isset($_POST['mythical'])) {
-						switch ($_POST['mythical']) {
-							//case 'mythical00a': include ("Roblox/TD/UTS/Tower/6mythical/.php"); break;
+					if (isset($_POST['divine'])) {
+						switch ($_POST['divine']) {
+							case 'divine00a': include ("Roblox/TD/UTS/Tower/8divine/bb.php"); break;
 						}
 					}
 				?>
