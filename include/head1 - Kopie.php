8baab06dfca0eb24b1d6608077adb01388e391c8
Kirito061294 <login@mwiese.de>
2022-10-06 19:45:04 +0200

    Update head1 - Kopie.php

--- include/head1 - Kopie.php
+++ include/head1 - Kopie.php
@@ -1,6 +1,7 @@
 <link rel="shortcut icon" href="https://mwiese.de/include/45454.ico" type="image/x-icon"/>
         <link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style.css">
         <meta lang="de" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+		<meta property="og:url" content="https://www.mwiese.de/" />
     </head>
 	<body>
         <header>
