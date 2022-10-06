caf7ecb54a9c5cbf8756fb1dd10e0bd2c5248d33
Kirito061294 <login@mwiese.de>
2022-10-06 19:44:33 +0200

    Update godly.php

--- Unterhaltung/Spiele/Roblox/TD/UTS/Tower/godly.php
+++ Unterhaltung/Spiele/Roblox/TD/UTS/Tower/godly.php
@@ -1,18 +1,18 @@
-<select name="godly">
+<select name="divine">
 									<?php
 						$selected = '';
 						if (isset($_POST['submit'])) {
-							$selected = $_POST['godly'];
+							$selected = $_POST['divine'];
 						}
 						echo '<option value="" disabled selected></option>';
-							//add_option($selected,'godly00a','Blackbeard');
+							add_option($selected,'divine00a','Blackbeard');
 						?>
 
 								</select>
 									<?php
-					if (isset($_POST['godly'])) {
-						switch ($_POST['godly']) {
-							//case 'godly00a': include ("Roblox/TD/UTS/Tower/7godly/.php"); break;
+					if (isset($_POST['divine'])) {
+						switch ($_POST['divine']) {
+							case 'divine00a': include ("Roblox/TD/UTS/Tower/8divine/bb.php"); break;
 						}
 					}
 				?>
