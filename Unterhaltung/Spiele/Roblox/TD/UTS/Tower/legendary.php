6771da2ef0f9738610b5432eae39ff648a75bc92
Kirito061294 <login@mwiese.de>
2022-10-06 19:44:31 +0200

    Update legendary.php

--- Unterhaltung/Spiele/Roblox/TD/UTS/Tower/legendary.php
+++ Unterhaltung/Spiele/Roblox/TD/UTS/Tower/legendary.php
@@ -1,18 +1,18 @@
-<select name="legendary">
+<select name="divine">
 									<?php
 						$selected = '';
 						if (isset($_POST['submit'])) {
-							$selected = $_POST['legendary'];
+							$selected = $_POST['divine'];
 						}
 						echo '<option value="" disabled selected></option>';
-							//add_option($selected,'legendary00a','');
+							add_option($selected,'divine00a','Blackbeard');
 						?>
 
 								</select>
 									<?php
-					if (isset($_POST['legendary'])) {
-						switch ($_POST['legendary']) {
-							//case 'legendary00a': include ("Roblox/TD/UTS/Tower/5legendary/.php"); break;
+					if (isset($_POST['divine'])) {
+						switch ($_POST['divine']) {
+							case 'divine00a': include ("Roblox/TD/UTS/Tower/8divine/bb.php"); break;
 						}
 					}
 				?>
