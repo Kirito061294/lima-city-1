550506d17771943548ceabde992b9165babeb742
Kirito061294 <login@mwiese.de>
2022-10-06 19:44:24 +0200

    Update uncommon.php

--- Unterhaltung/Spiele/Roblox/TD/UTS/Tower/uncommon.php
+++ Unterhaltung/Spiele/Roblox/TD/UTS/Tower/uncommon.php
@@ -1,18 +1,18 @@
-<select name="uncommon">
+<select name="divine">
 									<?php
 						$selected = '';
 						if (isset($_POST['submit'])) {
-							$selected = $_POST['uncommon'];
+							$selected = $_POST['divine'];
 						}
 						echo '<option value="" disabled selected></option>';
-							//add_option($selected,'uncommon00a','');
+							add_option($selected,'divine00a','Blackbeard');
 						?>
 
 								</select>
 									<?php
-					if (isset($_POST['uncommon'])) {
-						switch ($_POST['uncommon']) {
-							//case 'uncommon00a': include ("Roblox/TD/UTS/Tower/2uncommon/.php"); break;
+					if (isset($_POST['divine'])) {
+						switch ($_POST['divine']) {
+							case 'divine00a': include ("Roblox/TD/UTS/Tower/8divine/bb.php"); break;
 						}
 					}
 				?>
