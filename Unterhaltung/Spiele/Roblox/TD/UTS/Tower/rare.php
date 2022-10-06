8d37ffd4c6051f509f5762db0f6529bc3cf5aa8b
Kirito061294 <login@mwiese.de>
2022-10-06 19:44:27 +0200

    Update rare.php

--- Unterhaltung/Spiele/Roblox/TD/UTS/Tower/rare.php
+++ Unterhaltung/Spiele/Roblox/TD/UTS/Tower/rare.php
@@ -1,18 +1,18 @@
-<select name="rare">
+<select name="divine">
 									<?php
 						$selected = '';
 						if (isset($_POST['submit'])) {
-							$selected = $_POST['rare'];
+							$selected = $_POST['divine'];
 						}
 						echo '<option value="" disabled selected></option>';
-							//add_option($selected,'rare00a','');
+							add_option($selected,'divine00a','Blackbeard');
 						?>
 
 								</select>
 									<?php
-					if (isset($_POST['rare'])) {
-						switch ($_POST['rare']) {
-							//case 'rare00a': include ("Roblox/TD/UTS/Tower/3rare/.php"); break;
+					if (isset($_POST['divine'])) {
+						switch ($_POST['divine']) {
+							case 'divine00a': include ("Roblox/TD/UTS/Tower/8divine/bb.php"); break;
 						}
 					}
 				?>
