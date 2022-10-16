b6114dbb083e5cd5656db40bdbf082d792c2f1de
Kirito061294 <login@mwiese.de>
2022-10-16 17:59:05 +0200

    Update common.php

--- Unterhaltung/Spiele/Roblox/TD/UTS/Tower/common.php
+++ Unterhaltung/Spiele/Roblox/TD/UTS/Tower/common.php
@@ -16,6 +16,16 @@
 							add_option($selected,'common00i','Deku');
 							add_option($selected,'common00j','Flash');
 							add_option($selected,'common00k','Green Arrow');
+							add_option($selected,'common00l','Hawkeye');
+							add_option($selected,'common00m','Iron Man');
+							add_option($selected,'common00n','Luffy');
+							add_option($selected,'common00o','Luke Cage');
+							add_option($selected,'common00p','Nightwing');
+							add_option($selected,'common00q','Robin');
+							add_option($selected,'common00r','Sabretooth');
+							add_option($selected,'common00s','Spiderman');
+							add_option($selected,'common00t','War Machine');
+							add_option($selected,'common00u','Wonder Woman');
 						?>
 
 								</select>
@@ -33,6 +43,16 @@
 							case 'common00i': include ("Roblox/TD/UTS/Tower/1common/d.php"); break;
 							case 'common00j': include ("Roblox/TD/UTS/Tower/1common/f.php"); break;
 							case 'common00k': include ("Roblox/TD/UTS/Tower/1common/ga.php"); break;
+							case 'common00l': include ("Roblox/TD/UTS/Tower/1common/he.php"); break;
+							case 'common00m': include ("Roblox/TD/UTS/Tower/1common/im.php"); break;
+							case 'common00n': include ("Roblox/TD/UTS/Tower/1common/l.php"); break;
+							case 'common00o': include ("Roblox/TD/UTS/Tower/1common/lc.php"); break;
+							case 'common00p': include ("Roblox/TD/UTS/Tower/1common/nw.php"); break;
+							case 'common00q': include ("Roblox/TD/UTS/Tower/1common/r.php"); break;
+							case 'common00r': include ("Roblox/TD/UTS/Tower/1common/st.php"); break;
+							case 'common00s': include ("Roblox/TD/UTS/Tower/1common/sm.php"); break;
+							case 'common00t': include ("Roblox/TD/UTS/Tower/1common/wm.php"); break;
+							case 'common00u': include ("Roblox/TD/UTS/Tower/1common/ww.php"); break;
 						}
 					}
 				?>
