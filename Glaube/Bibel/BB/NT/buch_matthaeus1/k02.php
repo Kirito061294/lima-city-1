05e7f2e33676cfbf4af2a2697b0ff032fcb1a695
Kirito061294 <login@mwiese.de>
2022-10-18 19:09:34 +0200

    Create k02.php

---
+++ Glaube/Bibel/BB/NT/buch_matthaeus1/k02.php
@@ -0,0 +1,71 @@
+<div style="height: 25em; overflow-y: scroll;">
+									<div class ="verse">
+										<div class="zitat1"><p><b>Die Sterndeuter aus dem Osten</b></p><p>Vers 1: <?php include ("BB/NT/buch_matthaeus1/k02/v01.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 2: <?php include ("BB/NT/buch_matthaeus1/k02/v02.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 3: <?php include ("BB/NT/buch_matthaeus1/k02/v03.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 4: <?php include ("BB/NT/buch_matthaeus1/k02/v04.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 5: <?php include ("BB/NT/buch_matthaeus1/k02/v05.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 6: <?php include ("BB/NT/buch_matthaeus1/k02/v06.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 7: <?php include ("BB/NT/buch_matthaeus1/k02/v07.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 8: <?php include ("BB/NT/buch_matthaeus1/k02/v08.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 9: <?php include ("BB/NT/buch_matthaeus1/k02/v09.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 10: <?php include ("BB/NT/buch_matthaeus1/k02/v10.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 11: <?php include ("BB/NT/buch_matthaeus1/k02/v11.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 12: <?php include ("BB/NT/buch_matthaeus1/k02/v12.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p><b>Die Flucht nach Ägypten</b></p><p>Vers 13: <?php include ("BB/NT/buch_matthaeus1/k02/v13.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 14: <?php include ("BB/NT/buch_matthaeus1/k02/v14.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 15: <?php include ("BB/NT/buch_matthaeus1/k02/v15.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p><b>Herodes tötet die Kinder in Betlehem</b></p><p>Vers 16: <?php include ("BB/NT/buch_matthaeus1/k02/v16.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 17: <?php include ("BB/NT/buch_matthaeus1/k02/v17.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 18: <?php include ("BB/NT/buch_matthaeus1/k02/v18.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p><b>Die Rückkehr nach Nazaret</b></p><p>Vers 19: <?php include ("BB/NT/buch_matthaeus1/k02/v19.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 20: <?php include ("BB/NT/buch_matthaeus1/k02/v20.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 21: <?php include ("BB/NT/buch_matthaeus1/k02/v21.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 22: <?php include ("BB/NT/buch_matthaeus1/k02/v22.txt"); ?></p></div>
+									</div>
+									<div class ="verse">
+										<div class="zitat1"><p>Vers 23: <?php include ("BB/NT/buch_matthaeus1/k02/v23.txt"); ?></p></div>
+									</div>
+								</div>
