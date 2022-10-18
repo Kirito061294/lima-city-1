07240fb375ae371ff167d2e4fb69d4a379db3b10
Kirito061294 <login@mwiese.de>
2022-10-18 19:09:05 +0200

    Update head1.php

--- include/head1.php
+++ include/head1.php
@@ -6,29 +6,29 @@
         <header>
             <section>
                 <nav>
-				    <a class="logo" href="raspi.local:5000">Home</a>
+				    <a class="logo" href="https://mwiese.de">Home</a>
 				    <input type="checkbox" id="toggle_button">
 					<label for="toggle_button" class="toggle_button"><span class="bar"></span><span class="bar"></span><span class="bar"></span></label>
                     <ul class="kopf">
                         <li class="expandable_li">
 						    <input type="checkbox" id="unterhaltung_checkbox">
 							<label for="unterhaltung_checkbox">Unterhaltung</label>
-						    <ul class="dropdown"><li><a href="http://raspi.local:5000/Unterhaltung/Musik/Musik.php">Musik</a></li><li><a href="http://raspi.local:5000/Unterhaltung/Spiele/Spiele.php">Spiele</a></li></ul>
+						    <ul class="dropdown"><li><a href="https://mwiese.de/Unterhaltung/Musik/Musik.php">Musik</a></li><li><a href="http://raspi.local:5000/Unterhaltung/Spiele/Spiele.php">Spiele</a></li></ul>
 						</li>
                         <li class="expandable_li">
 						    <input type="checkbox" id="technik_checkbox">
 							<label for="technik_checkbox">Technik</label>
-						    <ul class="dropdown"><li><a href="http://raspi.local:5000/Technik/OS/OS.php">Betriebssysteme</a></li><li><a href="http://raspi.local:5000/Technik/Netzwerk/Netzwerk.php">Internet/Netzwerk</a></li><li><a href="http://raspi.local:5000/Technik/Interessantes.php">Interessantes</a></li></ul>
+						    <ul class="dropdown"><li><a href="https://mwiese.de/Technik/OS/OS.php">Betriebssysteme</a></li><li><a href="http://raspi.local:5000/Technik/Netzwerk/Netzwerk.php">Internet/Netzwerk</a></li><li><a href="http://raspi.local:5000/Technik/Interessantes.php">Interessantes</a></li></ul>
 						</li>
                         <li class="expandable_li">
 						    <input type="checkbox" id="spezielles_checkbox">
 							<label for="spezielles_checkbox">Spezielles</label>
-						    <ul class="dropdown"><li><a href="http://raspi.local:5000/spezielles/aussagen/aussagen.php">Aussagen</a></li></ul>
+						    <ul class="dropdown"><li><a href="https://mwiese.de/spezielles/aussagen/aussagen.php">Aussagen</a></li></ul>
 						</li>
                         <li class="expandable_li">
 						    <input type="checkbox" id="subdomains_checkbox">
 							<label for="subdomains_checkbox">Unterseiten</label>
-						    <ul class="dropdown"><li><a href="http://raspi.local:5000/Glaube/Glaube.php" target="_blank">Glaubensthemen</a></li><li><a href="https://pc.mwiese.de" target="_blank">Mein Hobby</a></li><li><a href="http://raspi.local:5000/Themen/Themen.php" target="_blank">Spezielle Themen</a></li></ul>
+						    <ul class="dropdown"><li><a href="https://mwiese.de/Glaube/Glaube.php" target="_blank">Glaubensthemen</a></li><li><a href="https://pc.mwiese.de" target="_blank">Mein Hobby</a></li><li><a href="http://raspi.local:5000/Themen/Themen.php" target="_blank">Spezielle Themen</a></li></ul>
 						</li>
                         <li><a href="https://mwiese.de/Rechtliches/Impressum.php" target="_blank">Impressum</a></li>
                         <li><a href="https://mwiese.de/Rechtliches/Datenschutz.php" target="_blank">Datenschutz</a></li>
