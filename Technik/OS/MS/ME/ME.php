b4764042b0290330db7318b731eb95af8aaf7585
Kirito061294 <login@mwiese.de>
2022-12-06 21:08:20 +0100

    Update ME.php

--- Technik/OS/MS/ME/ME.php
+++ Technik/OS/MS/ME/ME.php
@@ -1,2 +1,2 @@
-<p><a href="OS/MS/ME/ME_Tricks.php">Windows ME Tricks</a></p>
+<p><a href="MS/ME/ME_Tricks.php">Windows ME Tricks</a></p>
 						<p><a href="https://winworldpc.com/product/windows-me/final" target="_blank">Windows ME ISO</a></p>
