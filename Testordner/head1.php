56b041f2867a20e89ef6db04ff98f1c3cf18729b
Kirito061294 <login@mwiese.de>
2022-12-15 17:20:48 +0100

    Create head1.php

---
+++ Testordner/head1.php
@@ -0,0 +1,29 @@
+<link rel="shortcut icon" href="https://mwiese.de/include/45454.ico" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style.css">
+        <link rel="stylesheet" type="text/css" href="https://mwiese.de/Testordner/style.css">
+        <meta lang="de" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    </head>
+	<body>
+        <header>
+            <section>
+                <nav>
+				    <a class="logo" href="https://mwiese.de">Home</a>
+				    <input type="checkbox" id="toggle_button">
+					<label for="toggle_button" class="toggle_button"><span class="bar"></span><span class="bar"></span><span class="bar"></span></label>
+                    <ul class="kopf">
+                        <li class="expandable_li">
+						    <input type="checkbox" id="technik_checkbox">
+							<label for="technik_checkbox">Themen</label>
+						    <ul class="dropdown">
+								<li><a href="">Test1</a></li>
+								<li><a href="">Test2</a></li>
+								<li><a href="">Test3</a></li>
+							</ul>
+						</li>
+                        <li><a href="https://mwiese.de/Rechtliches/Impressum.php" target="_blank">Impressum</a></li>
+                        <li><a href="https://mwiese.de/Rechtliches/Datenschutz.php" target="_blank">Datenschutz</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </header>
+        <div class="center1">
