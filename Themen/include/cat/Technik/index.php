924b1778f9073cb55394d6d36762dc326c41b8a0
Kirito061294 <login@mwiese.de>
2022-12-15 17:25:13 +0100

    Update index.php

--- Themen/include/cat/Technik/index.php
+++ Themen/include/cat/Technik/index.php
@@ -1,22 +1,18 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-       "http://www.w3.org/TR/html4/loose.dtd">
+<select name="technik">
+						<?php
+						$selected = '';
+						if (isset($_POST['submit'])) {
+							$selected = $_POST['technik'];
+						}
+						echo '<option value="" disabled selected></option>';
+							add_option($selected,'technik00a','Raspberry Pi');
+						?>
 
-<html>
-    <head>
-        <title>Themen</title>
-        <?php include ("../include/head.php"); ?>
-    </head>
-    <body>
-	    <?php include ("../include/header.php"); ?>
-	    <div align="center">
-                <h1>Wilkommen!</h1>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-                <h3><a href="https://themen.mwiese.de">Zur Startseite</a></h3>
-            <br />
-        </div>
-    </body>
-</html>
+					</select>
+			<?php
+			    if (isset($_POST['technik'])) {
+                    switch ($_POST['technik']) {
+                        case 'technik00a': include ("include/cat/Technik/raspi.php"); break;
+                    }
+				}
+			?>
