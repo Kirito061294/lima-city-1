777d290b89e5c03adb8fca3127e6ee839cefdf5d
Kirito061294 <login@mwiese.de>
2023-01-14 20:31:09 +0100

    Update MMO.php

--- Unterhaltung/Spiele/Online/MMO.php
+++ Unterhaltung/Spiele/Online/MMO.php
@@ -1,3 +1,4 @@
+<p>Die Links hier verweisen auf externe Seiten. mwiese.de hat nichts mit selbigen zu tun.</p>
 <p><a href="http://de.nostale.gameforge.com/" target="_blank">Nostale</a></p>
 						<p><a href="http://de.elsword.gameforge.com/" target="_blank">Elsword</a></p>
 						<p><a href="http://de.metin2.gameforge.com/" target="_blank">Metin2</a></p>
