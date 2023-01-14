8df0a229106e1e1590e63074af7addbe94b91778
Kirito061294 <login@mwiese.de>
2023-01-14 21:08:53 +0100

    Update head1.php

--- Testordner/head1.php
+++ Testordner/head1.php
@@ -1,6 +1,6 @@
 <link rel="shortcut icon" href="https://mwiese.de/include/45454.ico" type="image/x-icon"/>
         <link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style.css">
-        <link rel="stylesheet" type="text/css" href="https://mwiese.de/Testordner/style.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
         <meta lang="de" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     </head>
 	<body>
