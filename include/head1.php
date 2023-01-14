14d3b41d48a7059169349c8ef8be60b59b13381a
Kirito061294 <login@mwiese.de>
2023-01-14 21:54:31 +0100

    Update head1.php

--- include/head1.php
+++ include/head1.php
@@ -1,5 +1,5 @@
 <link rel="shortcut icon" href="https://mwiese.de/include/45454.ico" type="image/x-icon"/>
-        <link rel="stylesheet" type="text/css" href="include/style.php">
+        <link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style.css">
         <meta lang="de" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     </head>
 	<body>
