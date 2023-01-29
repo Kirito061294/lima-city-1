222d4d30db2cdd9fee0c893bab2cdd0849493c19
Kirito061294 <login@mwiese.de>
2023-01-29 08:14:22 +0100

    Update Tests.php

--- Testordner/Tests.php
+++ Testordner/Tests.php
@@ -15,6 +15,7 @@
 					<div>
 						<input type="submit" name="submit" value="Auswahl">
                         <div>hallo1</div>
+						<input type="checkbox" id="toggle_button1" style="display:none;">
 						<label class="rechts_angezeigt" for="toggle_button1" class="toggle_button1"><span>Kommentare einblenden</span></label>
 					</div>
                     <div> hallo2</div>
