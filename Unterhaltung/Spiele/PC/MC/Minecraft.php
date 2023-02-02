7c31a961d8fa1313f666c3f15bf4380868bba103
Kirito061294 <login@mwiese.de>
2023-02-02 04:11:39 +0100

    Update Minecraft.php

--- Unterhaltung/Spiele/PC/MC/Minecraft.php
+++ Unterhaltung/Spiele/PC/MC/Minecraft.php
@@ -5,11 +5,9 @@
 							$selected = $_POST['minecraft'];
 						}
 						echo '<option value="" disabled selected></option>';
-							add_option($selected,'mc00c','Client Mods');
-							add_option($selected,'mc00a','Launcher');
+							add_option($selected,'mc00c','Forge Mods');
+							//add_option($selected,'mc00a','Launcher');
 							add_option($selected,'mc00b','Server Plugins');
-							//add_option($selected,'mc00d','Launcher');
-							//add_option($selected,'mc00e','Launcher');
 						?>
 
 						</select>
@@ -19,8 +17,6 @@
 							case 'mc00a': include ("PC/MC/L.php"); break;
 							case 'mc00b': include ("PC/MC/SP.php"); break;
 							case 'mc00c': include ("PC/MC/CM.php"); break;
-							//case 'mc00d': include ("PC/MC/L.php"); break;
-							//case 'mc00e': include ("PC/MC/L.php"); break;
 						}
 					}
 				?>
