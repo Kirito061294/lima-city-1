ddab8c22932c0b0d59fefede3431178458e487e3
Kirito061294 <login@mwiese.de>
2023-02-02 03:56:47 +0100

    Update 10.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/op/03/10.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/op/03/10.php
@@ -1,3 +1,3 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/ckoolMJBICQ" frameborder="0" allowfullscreen></iframe>
+								<iframe src="http://marcel1.local:3000/embed/ckoolMJBICQ"<?php include ("include/1.php"); ?>></iframe>
 							</div>
