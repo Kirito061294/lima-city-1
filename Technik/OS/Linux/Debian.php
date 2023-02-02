5f79f6b06cf592591a9a3d7587ac716b81160578
Kirito061294 <login@mwiese.de>
2023-02-02 03:51:17 +0100

    Update Debian.php

--- Technik/OS/Linux/Debian.php
+++ Technik/OS/Linux/Debian.php
@@ -5,7 +5,7 @@
 			<p><a href="" target="_blank"></a></p>
 			<p><a href="" target="_blank"></a></p>
 			<p><a href="" target="_blank"></a></p>
-			<div class="sub-mod">
+			<div class="sub-mod scroll_y">
 				<p>Abwandlungen</p>
 				<p><a href="" target="_blank"></a></p>
 			</div>
