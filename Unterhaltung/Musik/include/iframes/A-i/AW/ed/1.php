ca276e00f72de66946d445c2b7d8d5fe384ce586
Kirito061294 <login@mwiese.de>
2023-02-02 03:53:55 +0100

    Update 1.php

--- Unterhaltung/Musik/include/iframes/A-i/AW/ed/1.php
+++ Unterhaltung/Musik/include/iframes/A-i/AW/ed/1.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/Sr56Gp1_hME" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=HR8TZn-xKk4" target="_blank">Originaler Song</a></p>
+								<iframe src="http://marcel1.local:3000/embed/Sr56Gp1_hME"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="http://marcel1.local:3000/watch?v=HR8TZn-xKk4" target="_blank">Originaler Song</a></p>
 							</div>
