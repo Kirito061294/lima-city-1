57e53fb5189f4a536209afb4c7e196fe8009c33a
Kirito061294 <login@mwiese.de>
2023-02-02 03:56:14 +0100

    Update 7.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/op/02/7.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/op/02/7.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/-EN_FlepRL4" frameborder="0" allowfullscreen></iframe>
+								<iframe src="http://marcel1.local:3000/embed/-EN_FlepRL4"<?php include ("include/1.php"); ?>></iframe>
 								<p><a href="https://www.youtube.com/watch?v=4H_9itcwwFM" target="_blank">Originaler Song</a> - Daisy×Daisy - Evidence</p>
 							</div>
