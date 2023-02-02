052f98eecad972cb2acc25ae2c6e064e05b89658
Kirito061294 <login@mwiese.de>
2023-02-02 03:54:50 +0100

    Update 3.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/ed/3.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/ed/3.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/8w9LYWLDwIY" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=_d5gjBUncNk" target="_blank">Originaler Song</a> - Shiho Nanba - GOMEN NE, WATASHI</p>
+								<iframe src="http://marcel1.local:3000/embed/8w9LYWLDwIY"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="http://marcel1.local:3000/watch?v=_d5gjBUncNk" target="_blank">Originaler Song</a> - Shiho Nanba - GOMEN NE, WATASHI</p>
 							</div>
