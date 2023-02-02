0b7dc6f1b6788da7105a2b2fffeb5efc3e56c967
Kirito061294 <login@mwiese.de>
2023-02-02 03:54:42 +0100

    Update 17.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/ed/17.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/ed/17.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/Vc0zOvptba0" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=i57udDGhsfE" target="_blank">Originaler Song</a> - ROOT FIVE - Kimi no Mirai</p>
+								<iframe src="http://marcel1.local:3000/embed/Vc0zOvptba0"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="http://marcel1.local:3000/watch?v=i57udDGhsfE" target="_blank">Originaler Song</a> - ROOT FIVE - Kimi no Mirai</p>
 							</div>
