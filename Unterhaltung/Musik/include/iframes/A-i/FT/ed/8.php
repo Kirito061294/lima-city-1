1185d44223fe5b3926eb8f78e6b77bea38e7c997
Kirito061294 <login@mwiese.de>
2023-02-02 03:55:08 +0100

    Update 8.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/ed/8.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/ed/8.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/CmxgaMQC33E" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=mSwOvbMOr4Q" target="_blank">Originaler Song</a> - 25idoling - Don't think. Feel</p>
+								<iframe src="http://marcel1.local:3000/embed/CmxgaMQC33E"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="http://marcel1.local:3000/watch?v=mSwOvbMOr4Q" target="_blank">Originaler Song</a> - 25idoling - Don't think. Feel</p>
 							</div>
