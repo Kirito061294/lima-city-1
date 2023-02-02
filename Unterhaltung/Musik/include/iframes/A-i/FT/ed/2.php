91676f2c8d47059aeb7d9a9106dc24dedf8c2803
Kirito061294 <login@mwiese.de>
2023-02-02 03:54:46 +0100

    Update 2.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/ed/2.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/ed/2.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/GIszDdHKruI" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=w4X8cYeakPM" target="_blank">Originaler Song</a> - OneLifeCrew - Tsuioku Marry go round</p>
+								<iframe src="http://marcel1.local:3000/embed/GIszDdHKruI"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="http://marcel1.local:3000/watch?v=w4X8cYeakPM" target="_blank">Originaler Song</a> - OneLifeCrew - Tsuioku Marry go round</p>
 							</div>
