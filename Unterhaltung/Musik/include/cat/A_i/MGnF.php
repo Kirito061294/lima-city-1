9d13d4d36815dfd1f7e9f646b65d223a4c19881b
Kirito061294 <login@mwiese.de>
2023-02-02 03:52:44 +0100

    Update MGnF.php

--- Unterhaltung/Musik/include/cat/A_i/MGnF.php
+++ Unterhaltung/Musik/include/cat/A_i/MGnF.php
@@ -7,8 +7,8 @@
 							echo '<option value="" disabled selected></option>';
 								add_option($selected,'mgnf001','Opening 1');
 								add_option($selected,'mgnf00a','Ending 1');
-								//add_option($selected,'mgnf002','Opening 2');
-								//add_option($selected,'mgnf00b','Ending 2');
+								add_option($selected,'mgnf002','Opening 2');
+								add_option($selected,'mgnf00b','Ending 2');
 							?>
 
 						</select>
@@ -16,9 +16,9 @@
 			    if (isset($_POST['mgnf'])) {
                     switch ($_POST['mgnf']) {
                         case 'mgnf001': include ("include/iframes/A-i/MGnF/op/1.php"); break;
-                        //case 'mgnf002': include ("include/iframes/A-i/MGnF/op/2.php"); break;
+                        case 'mgnf002': include ("include/iframes/A-i/MGnF/op/2.php"); break;
                         case 'mgnf00a': include ("include/iframes/A-i/MGnF/ed/1.php"); break;
-                        //case 'mgnf00b': include ("include/iframes/A-i/MGnF/ed/2.php"); break;
+                        case 'mgnf00b': include ("include/iframes/A-i/MGnF/ed/2.php"); break;
                     }
 				}
 			?>
