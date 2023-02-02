6dec78b0341356326243bc3e4466719ffd26ae4e
Kirito061294 <login@mwiese.de>
2023-02-02 03:54:53 +0100

    Update 4.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/ed/4.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/ed/4.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/iab-W6B-4WI" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=npE8RBnBsAk" target="_blank">Originaler Song</a> - Mikuni Shimokawa - Kimi ga iru kara</p>
+								<iframe src="http://marcel1.local:3000/embed/iab-W6B-4WI"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="http://marcel1.local:3000/watch?v=npE8RBnBsAk" target="_blank">Originaler Song</a> - Mikuni Shimokawa - Kimi ga iru kara</p>
 							</div>
