f8a2fe47f4b78c27c8da7df70534ae1ef9507b48
Kirito061294 <login@mwiese.de>
2023-02-08 21:41:11 +0100

    Create 3.php

---
+++ Unterhaltung/Musik/include/iframes/A-i/SAO/op/3.php
@@ -0,0 +1,4 @@
+				<div class="video001">
+								<iframe src="https://www.youtube-nocookie.com/embed/6mCS9uL_YxU" frameborder="0" allowfullscreen></iframe>
+								<p><a href="https://www.youtube.com/watch?v=g8X_pBVVnA4" target="_blank">Originaler Song</a> - Petit Milady - Hi no Ito Rinne no Gemini</p>
+							</div>
