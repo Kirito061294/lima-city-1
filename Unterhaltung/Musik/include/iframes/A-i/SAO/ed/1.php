bce14db92b5c61f57ab899ea66b32bcc415915dd
Kirito061294 <login@mwiese.de>
2023-02-08 21:25:54 +0100

    Create 1.php

---
+++ Unterhaltung/Musik/include/iframes/A-i/SAO/ed/1.php
@@ -0,0 +1,4 @@
+				<div class="video001">
+								<iframe src="https://www.youtube-nocookie.com/embed/7AWwQxmMu6k" frameborder="0" allowfullscreen></iframe>
+								<p><a href="https://www.youtube.com/watch?v=U7ETmpacZOE" target="_blank">Originaler Song</a> - Tomatsu Haruka - Yume Sekai</p>
+							</div>
