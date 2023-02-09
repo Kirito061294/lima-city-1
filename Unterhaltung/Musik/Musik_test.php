b0990fa8286f8eb0a2c0f0d486c86088e5a3e951
Kirito061294 <login@mwiese.de>
2023-02-09 01:04:27 +0100

    Update Musik_test.php

--- Unterhaltung/Musik/Musik_test.php
+++ Unterhaltung/Musik/Musik_test.php
@@ -4,45 +4,6 @@
 
 <html>
     <head>
-	<script>
-    function dropdownMenu() {
-      document.getElementById("dropdown").classList.toggle("show");
-    }
-    window.onclick = function(event) {
-      if (!event.target.matches('.dropdown-button')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
-      }
-    }
-  </script>
-  <style>
-    .dropdown-content {
-      display: none;
-      position: absolute;
-      background-color: #f9f9f9;
-      min-width: 160px;
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      z-index: 1;
-    }
-    .dropdown-content a {
-      color: black;
-      padding: 12px 16px;
-      text-decoration: none;
-      display: block;
-    }
-    .dropdown-content a:hover {
-      background-color: #f1f1f1;
-    }
-    .show {
-      display: block;
-    }
-  </style>
         <title>Musik</title>
         <?php include ("../../include/head1.php"); ?>
             <div class="ueberschrift">
@@ -53,17 +14,29 @@
 				<p>Sollten rechtliche Probleme auftreten werde ich die betreffenden Videos statt sie einzubetten lediglich verlinken.</p>
 			</div>
             <div class="medienzone01">
-			<div class="dropdown">
-    <button onclick="dropdownMenu()" class="dropdown-button">Links</button>
-    <div id="dropdown" class="dropdown-content">
-      <a href="https://www.google.com">Google</a>
-      <a href="https://www.facebook.com">Facebook</a>
-      <a href="#">Sublink 1</a>
-      <a href="#">Sublink 2</a>
-    </div>
-  </div>
-				</div>
-            </form>
+				<link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style-test.css">
+				<form action="">
+					<?php
+					$directory = 'include/iframes/A-i/';
+					$files = scandir($directory);
+					foreach ($files as $file) {
+						if ($file != '.' && $file != '..') {
+							if (is_dir($directory . '/' . $file)) {
+								echo '			<input type="checkbox" name="' . $file . '">' . $file . '<br>'.PHP_EOL;
+								$sub_files = scandir($directory . '/' . $file);
+								foreach ($sub_files as $sub_file) {
+									if ($sub_file != '.' && $sub_file != '..') {
+										echo '			<input type="checkbox" name="' . $file . '/' . $sub_file . '">' . $sub_file . '<br>'.PHP_EOL;
+									}
+								}
+							} else {
+								echo '			<input type="checkbox" name="' . $file . '">' . $file . '<br>'.PHP_EOL;
+							}
+						}
+					}
+				?>
+				<input type="submit" value="Auswahl">
+				</form>
             </div>
 			<?php include ("../../include/footer1.php"); ?>
 </html>
