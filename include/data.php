7987986584d996e78e28aff749b605209465ca80
Kirito061294 <login@mwiese.de>
2023-02-11 22:22:13 +0100

    Update data.php

--- include/data.php
+++ include/data.php
@@ -1,18 +1,38 @@
-<?php
-// die Konstanten auslagern in eigene Datei
-// die dann per require_once ('konfiguration.php'); 
-// geladen wird.
 
-// Damit alle Fehler angezeigt werden
-// error_reporting(E_ALL);
-
-// Zum Aufbau der Verbindung zur Datenbank
-// die Daten erhalten Sie von Ihrem Provider
 define ( 'MYSQL_HOST', 'localhost' );
 
-// bei XAMPP ist der MYSQL_Benutzer: root
 define ( 'MYSQL_BENUTZER', 'USER348875_abcd' );
 define ( 'MYSQL_KENNWORT', 'OeyMNQyPklNN5j0fL8RzjgwJADITSbdw' );
-// für unser Bsp. nennen wir die DB db_348875_3
+
 define ( 'MYSQL_DATENBANK', 'db_348875_3' );
+<?php
+
+class Connection {
+
+    protected static $instance;
+
+    private static $dsn = 'mysql:host=localhost;dbname=db_348875_3';
+
+    private static $username = 'USER348875_abcd';
+
+    private static $password = 'OeyMNQyPklNN5j0fL8RzjgwJADITSbdw';
+
+    private function __construct() {
+        try {
+            self::$instance = new PDO(self::$dsn, self::$username, self::$password);
+        } catch (PDOException $e) {
+            echo "MySql Connection Error: " . $e->getMessage();
+        }
+    }
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            new Connection();
+        }
+
+        return self::$instance;
+    }
+
+}
+
 ?>
