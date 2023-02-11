d144c77750231badae1cd1b63547480208ad3d4d
Kirito061294 <login@mwiese.de>
2023-02-11 22:20:56 +0100

    Update Musik_test.php

--- Unterhaltung/Musik/Musik_test.php
+++ Unterhaltung/Musik/Musik_test.php
@@ -15,28 +15,38 @@
 			</div>
             <div class="medienzone01">
 				<link rel="stylesheet" type="text/css" href="https://mwiese.de/include/style-test.css">
-				<form action="">
-					<?php
-					$directory = 'include/iframes/A-i/';
-					$files = scandir($directory);
-					foreach ($files as $file) {
-						if ($file != '.' && $file != '..') {
-							if (is_dir($directory . '/' . $file)) {
-								echo '			<input type="checkbox" name="' . $file . '">' . $file . '<br>'.PHP_EOL;
-								$sub_files = scandir($directory . '/' . $file);
-								foreach ($sub_files as $sub_file) {
-									if ($sub_file != '.' && $sub_file != '..') {
-										echo '			<input type="checkbox" name="' . $file . '/' . $sub_file . '">' . $sub_file . '<br>'.PHP_EOL;
-									}
-								}
-							} else {
-								echo '			<input type="checkbox" name="' . $file . '">' . $file . '<br>'.PHP_EOL;
-							}
-						}
-					}
-				?>
-				<input type="submit" value="Auswahl">
-				</form>
+				<ul><!--
+					<li class="expandable_list"><input type="checkbox" id="i1"><label for="i1">Adele</label>
+						<ul class="dropdown1"><?php //include ("include/categories/Adele.php"); ?></ul>
+					</li>
+					<li class="expandable_list"><input type="checkbox" id="i2"><label for="i2">Adel Tawil</label>
+						<ul class="dropdown1"><?php //include ("include/categories/Adel-Tawil.php"); ?></ul>
+					</li>
+					<li class="expandable_list"><input type="checkbox" id="i3"><label for="i3">Alan Walker</label>
+						<ul class="dropdown1"><?php //include ("include/categories/Alan-Walker.php"); ?></ul>
+					</li>-->
+					<li class="expandable_list"><input type="checkbox" id="i4"><label for="i4">Anime</label>
+						<ul class="dropdown1"><?php include ("include/categories/Anime.php"); ?></ul>
+					</li><!--
+					<li class="expandable_list"><input type="checkbox" id="i5"><label for="i5">Damestream</label>
+						<ul class="dropdown1"><?php //include ("include/categories/Damestream.php"); ?></ul>
+					</li>-->
+					<li class="expandable_list"><input type="checkbox" id="i6"><label for="i6">Geometry Dash</label>
+						<ul class="dropdown1"><?php include ("include/categories/GeometryDash.php"); ?></ul>
+					</li><!--
+					<li class="expandable_list"><input type="checkbox" id="i7"><label for="i7">George Ezra</label>
+						<ul class="dropdown1"><?php //include ("include/categories/George-Ezra.php"); ?></ul>
+					</li>-->
+					<li class="expandable_list"><input type="checkbox" id="i8"><label for="i8">Nightcore</label>
+						<ul class="dropdown1"><?php include ("include/categories/Nightcore.php"); ?></ul>
+					</li><!--
+					<li class="expandable_list"><input type="checkbox" id="i9"><label for="i9">Sunrise Avenue</label>
+						<ul class="dropdown1"><?php //include ("include/categories/Sunrise-Avenue.php"); ?></ul>
+					</li>
+					<li class="expandable_list"><input type="checkbox" id="i10"><label for="i10">Anderes</label>
+						<ul class="dropdown1"><?php //include ("include/categories/Anderes.php"); ?></ul>
+					</li>-->
+				</ul>
             </div>
 			<?php include ("../../include/footer1.php"); ?>
 </html>
