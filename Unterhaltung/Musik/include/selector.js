a5eaa2db59585eb75eb8ab97fbefcee440869352
Kirito061294 <login@mwiese.de>
2023-02-13 23:48:57 +0100

    Create selector.js

---
+++ Unterhaltung/Musik/include/selector.js
@@ -0,0 +1,29 @@
+<script type="application/json" data-drupal-selector="drupal-settings-json"> {
+	"path": {
+		"baseUrl":"\/","scriptPath":null,"pathPrefix":"de\/","currentPath":"node\/19891","currentPathIsAdmin":false,"isFront":false,"currentLanguage":"de"
+	},
+	"pluralDelimiter":"\u0003","suppressDeprecationErrors":true,"ajaxPageState": {
+		"libraries":"amd\/amd-analytics,amd\/amd-scripts,amd\/animate-on-scroll,amd\/global-styling,amd_core\/analytics_secondary,amd_core\/forms,amd_enterprise_recaptcha\/google-enterprise-recaptcha,amd_shop_product\/direct-buy,amd_shop_product\/direct-buy-analytics,amd_shop_product\/set-cart-token,amd_shop_product\/shopping-cart-actions,amd_support\/support_picker,captcha\/base,chosen\/drupal.chosen,chosen_lib\/chosen.css,classy\/node,core\/drupal.collapse,core\/internal.jquery.form,cshs\/cshs.base,paragraphs\/drupal.paragraphs.unpublished,system\/base,webform\/webform.ajax,webform\/webform.element.details.save,webform\/webform.element.details.toggle,webform\/webform.element.flexbox,webform\/webform.element.message,webform\/webform.element.select,webform\/webform.form,webform\/webform.javascript.footer_newsletter","theme":"amd","theme_token":null
+	},
+	"ajaxTrustedUrl": {
+		"form_action_p_pvdeGsVG5zNF_XLGPTvYSKCf43t8qZYSwcfZl2uzM":true,"\/de\/support?ajax_form=1":true,"\/de\/search":true},"chosen": {
+			"selector":"select[multiple=\u0027multiple\u0027],select.facets-dropdown,.embedded-catalog-entity-form select#edit-field-company,.embedded-catalog-entity-form select#edit-field-form-factor,select#edit-field-brand,select#edit-field-memory-ic","minimum_single":0,"minimum_multiple":0,"minimum_width":200,"use_relative_width":false,"options": {
+				"disable_search":false,"disable_search_threshold":0,"allow_single_deselect":false,"search_contains":true,"placeholder_text_multiple":"Choose some options","placeholder_text_single":"Choose an option","no_results_text":"No results match","max_shown_results":null,"inherit_select_classes":true
+			}
+		},"ajax": {
+			"edit-actions-submit": {
+				"callback":"::submitAjaxForm","event":"click","effect":"fade","speed":500,"progress": {
+					"type":"throbber","message":""
+				},"disable-refocus":true,"url":"\/de\/support?ajax_form=1","dialogType":"ajax","submit": {
+					"_triggering_element_name":"op","_triggering_element_value":"Jetzt registrieren"
+				}
+			}
+		},"cshs": {
+			"edit-product-type": {
+				"labels":[],"noneLabel":"- Bitte w\u00e4hlen -","noneValue":"All","noFirstLevelNone":false
+			}
+		},"drlocale":"en-us","drjs_token":"pk_26aa3e31250049b2939ee19b3472d245","user": {
+			"uid":0,"permissionsHash":"04b56e4a94694bd9cceba13786e0ce8a5e9ce774b2505af756ad6bc8ced7baa4"
+		}
+	}
+</script>
