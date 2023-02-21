785fd08c713c5d6a369ff582caa6b719c6390117
Kirito061294 <login@mwiese.de>
2023-02-21 18:10:12 +0100

    Update Songtitel.php

--- Unterhaltung/Musik/include/Songtitel.php
+++ Unterhaltung/Musik/include/Songtitel.php
@@ -3,30 +3,15 @@
 	$connection = Connection::getInstance();
 	
 	// Bei Klick auf eine Kategorie wenn Interpret ausgewählt:
-	if isset($_POST["Kategorie"]) {
-		echo 'struktur unfertig';
-//		$query = "SELECT * From Songs WHERE InterpretID =:interpreten AND catID =:category";
-//
-//		$statement = $connection->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-//		$statement->execute([":interpreten"=>$_POST["Interpret"],":category"=>$_POST["Kategorien"]]);
-//		$data=$statement->fetchAll();
-//		$array=[];
-//		foreach($data as $row) {
-//			$array[]=[$row["SongID"],$row["Songs"]];
-//			echo '                	<option value="' . $row['SongID'] . '">' . $row['Songs'] . '</option>'.PHP_EOL;
-		}
-	// Bei Klick auf Interpreten:
-	} else {
-		$query = "SELECT * From Kategorien WHERE InterpretID =:interpreten";
+	//echo 'struktur unfertig';
+	$query = "SELECT * From Songs WHERE InterpretID =:interpreten AND catID =:category";
 
-		$statement = $connection->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-		$statement->execute([":interpreten"=>$_POST["Interpret"]]);
-		$data=$statement->fetchAll();
-		$array=[];
-		foreach($data as $row) {
-			$array[]=[$row["catID"],$row["Kategorien"]];
-			echo '                	<option onClick="selectKategorien(this)"  value="' . $row['catID'] . '">' . $row['Kategorien'] . '</option>'.PHP_EOL;
-		}
+	$statement = $connection->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+	$statement->execute([":interpreten"=>$_POST["Interpret"],":category"=>$_POST["Kategorien"]]);
+	$data=$statement->fetchAll();
+	$array=[];
+	foreach($data as $row) {
+		$array[]=[$row["SongID"],$row["Song"]];
+		echo '                	<option value="' . $row['SongID'] . '">' . $row['Songs'] . '</option>'.PHP_EOL;
 	}
-	
 ?>
