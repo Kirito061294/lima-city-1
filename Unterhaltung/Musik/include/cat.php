cbbf1b3e12ea76d9952a68b902e9db72fe60a649
Kirito061294 <login@mwiese.de>
2023-02-21 18:10:02 +0100

    Create cat.php

---
+++ Unterhaltung/Musik/include/cat.php
@@ -0,0 +1,16 @@
+<?php
+	require_once ("../../../include/data.php");
+	$connection = Connection::getInstance();
+
+	// Bei Klick auf Interpreten:
+	$query = "SELECT * From Kategorien WHERE InterpretID =:interpreten";
+
+	$statement = $connection->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+	$statement->execute([":interpreten"=>$_POST["Interpret"]]);
+	$data=$statement->fetchAll();
+	$array=[];
+	foreach($data as $row) {
+		$array[]=[$row["catID"],$row["KategorieName"]];
+		echo '                	<option onClick="selectCat(this)"  value="' . $row['catID'] . '">' . $row['KategorieName'] . '</option>'.PHP_EOL;
+	}
+?>
