39278d96e9d01dcf06d6779a1f0438c5bb3fc5c6
Kirito061294 <login@mwiese.de>
2023-02-21 19:03:53 +0100

    Update Impressum.php

--- Rechtliches/Impressum.php
+++ Rechtliches/Impressum.php
@@ -4,24 +4,10 @@
 <html>
     <head>
         <title>mwiese.de</title>
-    <?php include ("../include/head1.php"); ?>
+		<?php include ("../include/head1.php"); ?>
             <div class="ueberschrift">
-                <h1>Informationen zur Seite</h1>
+                <h1>Impressum</h1>
 			</div>
-                <p>Kontaktdaten des Seitenbesitzers Marcel Wiese:</p>
-                <p>Adresse:</p>
-                <p>Marcel Wiese<br />
-				Dorfstraße 42,<br />
-                37176 Nörten-Hardenberg<br />
-				Tel: 015175001889<br />
-                E-Mail Adresse: kontakt@mwiese.de</p>
-            <br />
-				<p>
-				Diese Website erhebt von sich aus keinerlei Nutzer- oder Browserdaten.<br />
-				Das einzige was der Provider dieser Domain (<a href="https://lima-city.de">Lima-City.de</a>) an Daten erhebt ist die anzahl der einzelnen Seitenaufrufe.<br />
-				Sollten auf nachfolgenden Seiten anderweitig Daten erhoben werden, weise ich darauf hin.
-				</p>
-            <br />
-        </div>
-    </body>
+			<?php include ("Impressum1.php");
+			include ("../include/footer1.php"); ?>
 </html>
