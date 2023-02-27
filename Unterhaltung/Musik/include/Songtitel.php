f098070fbf26302f855077a8f93041adf4898e62
Kirito061294 <login@mwiese.de>
2023-02-27 14:25:34 +0100

    Update Songtitel.php

--- Unterhaltung/Musik/include/Songtitel.php
+++ Unterhaltung/Musik/include/Songtitel.php
@@ -3,14 +3,14 @@
 	$connection = Connection::getInstance();
 	
 	// Bei Klick auf eine Kategorie wenn Interpret ausgewählt:
-	$query = "SELECT * From Songs WHERE InterpretID =:interpreten AND catID =:category";
+	$query = "SELECT * From Songs WHERE catID =:category";
 
 	$statement = $connection->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-	$statement->execute([":interpreten"=>$_POST["Interpret"],":category"=>$_POST["Kategorien"]]);
+	$statement->execute([":category"=>$_POST["Kategorien"]]);
 	$data=$statement->fetchAll();
 	$array=[];
 	foreach($data as $row) {
 		$array[]=[$row["SongID"],$row["SongName"]];
-		echo '                	<option value="' . $row['SongID'] . '">' . $row['SongName'] . '</option>'.PHP_EOL;
+		echo '                	<option onClick="selectSong(this)" value="' . $row['SongID'] . '">' . $row['SongName'] . '</option>'.PHP_EOL;
 	}
 ?>
