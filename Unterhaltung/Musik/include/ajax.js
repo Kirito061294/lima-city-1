979bbbe5177cda85d5e7e94231e0d56fa79784f0
Kirito061294 <login@mwiese.de>
2023-02-27 14:25:11 +0100

    Update ajax.js

--- Unterhaltung/Musik/include/ajax.js
+++ Unterhaltung/Musik/include/ajax.js
@@ -27,3 +27,19 @@ function selectCat(event) {
     console.log(event.value);
 	async_getSongs(event.value)
 }
+
+
+function async_getPlayer(catID,InterpretID,SongID) {
+	$.ajax({
+		url: 'include/Player.php',
+		data: {'Interpret': InterpretID,'Kategorien': catID,'Songs': SongID},
+		type: 'POST',
+		success: function (data)  {
+			document.getElementById("player").innerHTML=data
+		}
+	});
+}
+function selectSong(event) {
+    console.log(event.value);
+	async_getPlayer(event.value)
+}
