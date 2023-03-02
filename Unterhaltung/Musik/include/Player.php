2f06b7bb8e93b04474a418262d31663e06083995
Kirito061294 <login@mwiese.de>
2023-03-02 18:45:06 +0100

    Update Player.php

--- Unterhaltung/Musik/include/Player.php
+++ Unterhaltung/Musik/include/Player.php
@@ -3,7 +3,9 @@
 	$connection = Connection::getInstance();
 	
 	// Bei Klick auf eine Kategorie wenn Interpret ausgewählt:
-	$query = "SELECT * From LinkListe WHERE SongID =:link";
+	$embed = "https://www.youtube-nocookie.com/embed/";
+	$external = "https://www.youtube.com/";
+	$query = "SELECT * FROM Songs WHERE SongID =:link";
 
 	$statement = $connection->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
 	$statement->execute([":link"=>$_POST["Songs"]]);
@@ -11,6 +13,9 @@
 	$array=[];
 	foreach($data as $row) {
 		$array[]=[$row["SongURL"]];
-		echo '					<iframe src="' . htmlspecialchars($row['SongURL']) . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'.PHP_EOL;
+		echo '<iframe src="';
+		echo $embed;
+		echo htmlspecialchars($row['SongURL']);
+		echo '" frameborder="0" allowfullscreen></iframe>'.PHP_EOL;
 	}
 ?>
