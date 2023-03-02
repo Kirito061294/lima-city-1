38279aa67a33e232937a351feb0910f55e95ef98
Kirito061294 <login@mwiese.de>
2023-03-02 18:44:46 +0100

    Update 2.php

--- Unterhaltung/Musik/include/iframes/A-i/MGnF/op/2.php
+++ Unterhaltung/Musik/include/iframes/A-i/MGnF/op/2.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/IGRAfhYEYKM" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=Qn8dJA3WM3M" target="_blank">Originaler Song</a></p>
+								<iframe src="<?php echo $embed; ?>hiopy_jxPIU" frameborder="0" allowfullscreen></iframe>
+								<p><a href="<?php echo $external; ?>" target="_blank">Originaler Song</a></p>
 							</div>
