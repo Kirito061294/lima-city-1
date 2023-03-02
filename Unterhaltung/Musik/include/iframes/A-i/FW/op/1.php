b489afaf0307440d77a845600163a6f71480a4ca
Kirito061294 <login@mwiese.de>
2023-03-02 18:44:25 +0100

    Update 1.php

--- Unterhaltung/Musik/include/iframes/A-i/FW/op/1.php
+++ Unterhaltung/Musik/include/iframes/A-i/FW/op/1.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/vV3l_1j8pPE" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=mFYK47afoSY" target="_blank">Originaler Song</a> - Ultra Tower - Kibō no Uta</p>
+								<iframe src="<?php echo $embed; ?>vV3l_1j8pPE" frameborder="0" allowfullscreen></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=mFYK47afoSY" target="_blank">Originaler Song</a> - Ultra Tower - Kibō no Uta</p>
 							</div>
