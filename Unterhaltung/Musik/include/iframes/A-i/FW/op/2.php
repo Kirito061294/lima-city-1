ddf58c9f89b559610556a497bbc5120a23a41c94
Kirito061294 <login@mwiese.de>
2023-03-02 18:44:27 +0100

    Update 2.php

--- Unterhaltung/Musik/include/iframes/A-i/FW/op/2.php
+++ Unterhaltung/Musik/include/iframes/A-i/FW/op/2.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/WGASdBxpVPw" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=xE0KsfXT9mo" target="_blank">Originaler Song</a> - Misokkasu - Rising Rainbow</p>
+								<iframe src="<?php echo $embed; ?>WGASdBxpVPw" frameborder="0" allowfullscreen></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=xE0KsfXT9mo" target="_blank">Originaler Song</a> - Misokkasu - Rising Rainbow</p>
 							</div>
