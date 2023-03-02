07b10cd21271e461e430fe3d538fab3d371edced
Kirito061294 <login@mwiese.de>
2023-03-02 18:44:22 +0100

    Update 7.php

--- Unterhaltung/Musik/include/iframes/A-i/FW/ed/7.php
+++ Unterhaltung/Musik/include/iframes/A-i/FW/ed/7.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/mPj7YeKJiJ4" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=m3kLWx_a5rg" target="_blank">Originaler Song</a> - Mai Fuchigami - Crossing Road</p>
+								<iframe src="<?php echo $embed; ?>mPj7YeKJiJ4" frameborder="0" allowfullscreen></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=m3kLWx_a5rg" target="_blank">Originaler Song</a> - Mai Fuchigami - Crossing Road</p>
 							</div>
