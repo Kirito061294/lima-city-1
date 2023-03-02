e272d428bfd57d0ababd0240e8b90baa88932223
Kirito061294 <login@mwiese.de>
2023-03-02 18:44:35 +0100

    Update 5.php

--- Unterhaltung/Musik/include/iframes/A-i/FW/op/5.php
+++ Unterhaltung/Musik/include/iframes/A-i/FW/op/5.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/RG36jRZwAP4" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=XgoHifs0sfk" target="_blank">Originaler Song</a> - Luck Life - Symbol</p>
+								<iframe src="<?php echo $embed; ?>RG36jRZwAP4" frameborder="0" allowfullscreen></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=XgoHifs0sfk" target="_blank">Originaler Song</a> - Luck Life - Symbol</p>
 							</div>
