c2461297cb40c002969b909f17b790b978422b8d
Kirito061294 <login@mwiese.de>
2023-03-02 18:42:59 +0100

    Update 9.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/ed/9.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/ed/9.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="http://marcel1.local:3000/embed/KWjFungsJFw"<?php include ("include/1.php"); ?>></iframe>
-								<p><a href="http://marcel1.local:3000/watch?v=XDLi5u4zs1Q" target="_blank">Originaler Song</a> - HI-FI CAMP - Kono te nobashite</p>
+								<iframe src="<?php echo $embed; ?>KWjFungsJFw"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=XDLi5u4zs1Q" target="_blank">Originaler Song</a> - HI-FI CAMP - Kono te nobashite</p>
 							</div>
