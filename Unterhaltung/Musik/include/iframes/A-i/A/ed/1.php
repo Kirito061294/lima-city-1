144497057941203cebb705522b0f12c31f1bed9c
Kirito061294 <login@mwiese.de>
2023-03-02 18:41:52 +0100

    Update 1.php

--- Unterhaltung/Musik/include/iframes/A-i/A/ed/1.php
+++ Unterhaltung/Musik/include/iframes/A-i/A/ed/1.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="http://marcel1.local:3000/embed/lE7Jz67etzA"<?php include ("include/1.php"); ?>></iframe>
-								<p><a href="http://marcel1.local:3000/watch?v=eErndXBEscc" target="_blank">Originaler Song</a></p>
+								<iframe src="<?php echo $embed; ?>lE7Jz67etzA"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=eErndXBEscc" target="_blank">Originaler Song</a></p>
 							</div>
