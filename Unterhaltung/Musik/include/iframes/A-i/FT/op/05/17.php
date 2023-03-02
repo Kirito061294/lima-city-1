1ab0bc3f7b78b4a558fc28aa1e08e8a44b095ce0
Kirito061294 <login@mwiese.de>
2023-03-02 18:43:56 +0100

    Update 17.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/op/05/17.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/op/05/17.php
@@ -1,3 +1,3 @@
 				<div class="video001">
-								<iframe src="http://marcel1.local:3000/embed/qpvkFgW109I"<?php include ("include/1.php"); ?>></iframe>
+								<iframe src="<?php echo $embed; ?>qpvkFgW109I"<?php include ("include/1.php"); ?>></iframe>
 							</div>
