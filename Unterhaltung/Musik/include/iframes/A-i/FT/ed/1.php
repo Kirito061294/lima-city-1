2325cc1364320c21c61080601aa878442ed5c4b7
Kirito061294 <login@mwiese.de>
2023-03-02 18:42:07 +0100

    Update 1.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/ed/1.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/ed/1.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="http://marcel1.local:3000/embed/4APiyhwzWoM"<?php include ("include/1.php"); ?>></iframe>
-								<p><a href="http://marcel1.local:3000/watch?v=MpwA_GNEloc" target="_blank">Originaler Song</a> - Warota - Kanpeki guu no ne</p>
+								<iframe src="<?php echo $embed; ?>4APiyhwzWoM"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=MpwA_GNEloc" target="_blank">Originaler Song</a> - Warota - Kanpeki guu no ne</p>
 							</div>
