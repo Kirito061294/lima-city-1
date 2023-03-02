e5d61033fdb34c4526d67e7ff09976e7f4bc926e
Kirito061294 <login@mwiese.de>
2023-03-02 18:44:10 +0100

    Update 2.php

--- Unterhaltung/Musik/include/iframes/A-i/FW/ed/2.php
+++ Unterhaltung/Musik/include/iframes/A-i/FW/ed/2.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/EVz82npkUyw" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=U4LR7ZJUwxs" target="_blank">Originaler Song</a> - Seiko Oomori - Sacchan no sexy Curry</p>
+								<iframe src="<?php echo $embed; ?>EVz82npkUyw" frameborder="0" allowfullscreen></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=U4LR7ZJUwxs" target="_blank">Originaler Song</a> - Seiko Oomori - Sacchan no sexy Curry</p>
 							</div>
