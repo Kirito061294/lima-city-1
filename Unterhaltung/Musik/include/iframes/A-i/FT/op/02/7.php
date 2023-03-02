7be7032d2632390cde95be619420c24b84c697da
Kirito061294 <login@mwiese.de>
2023-03-02 18:43:21 +0100

    Update 7.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/op/02/7.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/op/02/7.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="http://marcel1.local:3000/embed/-EN_FlepRL4"<?php include ("include/1.php"); ?>></iframe>
-								<p><a href="https://www.youtube.com/watch?v=4H_9itcwwFM" target="_blank">Originaler Song</a> - Daisy×Daisy - Evidence</p>
+								<iframe src="<?php echo $embed; ?>-EN_FlepRL4"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=4H_9itcwwFM" target="_blank">Originaler Song</a> - Daisy×Daisy - Evidence</p>
 							</div>
