4f515d57e09941e0492999897c9b7ba4dc15ffc2
Kirito061294 <login@mwiese.de>
2023-03-02 18:42:43 +0100

    Update 5.php

--- Unterhaltung/Musik/include/iframes/A-i/FT/ed/5.php
+++ Unterhaltung/Musik/include/iframes/A-i/FT/ed/5.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="http://marcel1.local:3000/embed/JzfWLfqG7eY"<?php include ("include/1.php"); ?>></iframe>
-								<p><a href="http://marcel1.local:3000/watch?v=Xm4lpatRGD0" target="_blank">Originaler Song</a> - Daisy x Daisy - Holy Shine</p>
+								<iframe src="<?php echo $embed; ?>JzfWLfqG7eY"<?php include ("include/1.php"); ?>></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=Xm4lpatRGD0" target="_blank">Originaler Song</a> - Daisy x Daisy - Holy Shine</p>
 							</div>
