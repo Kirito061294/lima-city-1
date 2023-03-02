88d9eb9c84e21487ab6c6f36377e32ece99c8d12
Kirito061294 <login@mwiese.de>
2023-03-02 18:44:07 +0100

    Update 1.php

--- Unterhaltung/Musik/include/iframes/A-i/FW/ed/1.php
+++ Unterhaltung/Musik/include/iframes/A-i/FW/ed/1.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/M_cyl6c8pMI" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=kCv3LcmD8e0" target="_blank">Originaler Song</a> - Tokyo Karankoron - Spice</p>
+								<iframe src="<?php echo $embed; ?>M_cyl6c8pMI" frameborder="0" allowfullscreen></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=kCv3LcmD8e0" target="_blank">Originaler Song</a> - Tokyo Karankoron - Spice</p>
 							</div>
