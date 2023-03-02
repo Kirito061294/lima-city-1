33f9952d3f1a147c1090fafed3f6f687397eba80
Kirito061294 <login@mwiese.de>
2023-03-02 18:44:41 +0100

    Update 7.php

--- Unterhaltung/Musik/include/iframes/A-i/FW/op/7.php
+++ Unterhaltung/Musik/include/iframes/A-i/FW/op/7.php
@@ -1,4 +1,4 @@
 				<div class="video001">
-								<iframe src="https://www.youtube-nocookie.com/embed/k1_djjTVwwY" frameborder="0" allowfullscreen></iframe>
-								<p><a href="https://www.youtube.com/watch?v=fAoRpLbJSVU" target="_blank">Originaler Song</a> - nano.RIPE - Last Chapter</p>
+								<iframe src="<?php echo $embed; ?>k1_djjTVwwY" frameborder="0" allowfullscreen></iframe>
+								<p><a href="<?php echo $external; ?>watch?v=fAoRpLbJSVU" target="_blank">Originaler Song</a> - nano.RIPE - Last Chapter</p>
 							</div>
