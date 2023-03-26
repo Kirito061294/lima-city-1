1235a90c3a6a85201f53d9af3ef57171f7407fd0
Kirito061294 <login@mwiese.de>
2023-03-26 18:56:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 	    <title>mwiese.de</title>
     <?php include ("include/head1.php"); ?>
             <div class="ueberschrift">
-			    <h1>Informationen zur Seite</h1>
+			    <h1 style="font-size: 150%;">Herzlich Wilkommen auf mwiese.de</h1>
 			</div>
-			<div class="info">
+			<div id="info" style="display: block;">
                 <p>Diese Seite ist ein Hobby und daher kann es sein das es dauert bis neues hinzugefügt wird.</p>
 				<p>Zusätzlich sei gesagt, das diese Website von sich aus keine Browserdaten der Nutzer erhebt.</p>
 				<p>Das einzige was der Provider dieser Domain (die TrafficPlex GmbH) an Daten erhebt ist die anzahl der einzelnen Seitenaufrufe.</p>
