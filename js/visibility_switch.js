66bbc3a91a4ddbcdc4b80a1bfc1371b6b1b2d36a
Kirito061294 <login@mwiese.de>
2023-03-26 18:56:34 +0200

    Create visibility_switch.js

---
+++ js/visibility_switch.js
@@ -0,0 +1,8 @@
+function toggleBlock(id) {
+  var text = document.getElementById(id);
+  text.style.display = (text.style.display === "none") ? "block" : "none";
+}
+function toggleGrid(id) {
+  var text = document.getElementById(id);
+  text.style.display = (text.style.display === "none") ? "grid" : "none";
+}
