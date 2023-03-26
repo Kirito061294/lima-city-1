3e063579f09655fe06cb463c9a84088750a53f09
Kirito061294 <login@mwiese.de>
2023-03-26 18:54:59 +0200

    Update footer1.php

--- include/footer1.php
+++ include/footer1.php
@@ -1,10 +1,10 @@
 <div class="ende1">
-				<a href="javascript:history.back()">Zurück</a>
+				<!--<a href="javascript:history.back()">Zurück</a>-->
 				<div class="abstand1"></div>
 			</div>
 		</div>
     </body>
 	<footer>
         <p>Author: Marcel Wiese</p>
-        <p>Copyright: mwiese.de 2018 - 2023</p>
+        <p>Copyright: <a href="https://mwiese.de">mwiese.de</a> 2018 - 2023</p>
     </footer>
