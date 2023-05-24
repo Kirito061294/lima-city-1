01840bdc068c6dc6239317d9c2869aa201720cc2
Kirito061294 <login@mwiese.de>
2023-05-24 03:24:40 +0200

    Update Spiele.php

--- Unterhaltung/Spiele/Spiele.php
+++ Unterhaltung/Spiele/Spiele.php
@@ -14,46 +14,46 @@
                 <p>Die meisten Dateilinks stammen nicht von mir und ich habe keinen Kontakt zum Uploader.</p>
                 <p>Ich übernehme keine verantwortung für eventuell auftretende Schwierigkeiten</p>
             </div>
-			<button id="myButton" class="footer-bg" onclick="toggleInfo()">Rechtliches</button>
-            <div class="linkzone01">
-			<form action="" method="post" class="mb-4">
-				<div><input type="submit" name="submit" value="Auswahl"></div>
-				<div class="include1">
-			    <select name="spiele">
-					<?php
-					$selected = '';
-					function add_option($selected, $option_value,$interpret_name) {
-						echo '<option value="'.$option_value.'"';
-						if ($selected == $option_value) {
-							echo ' selected="selected"';
+			<button class="knopf" onclick="toggleInfo()">Rechtliches</button>
+            <div class="main_content">
+				<form action="" method="post" class="mb-4">
+					<div><input type="submit" name="submit" value="Auswahl"></div>
+					<div class="include1">
+					<select name="spiele">
+						<?php
+						$selected = '';
+						function add_option($selected, $option_value,$interpret_name) {
+							echo '<option value="'.$option_value.'"';
+							if ($selected == $option_value) {
+								echo ' selected="selected"';
+							}
+							echo '>'.$interpret_name.'</option>';
 						}
-						echo '>'.$interpret_name.'</option>';
-					}
-					if (isset($_POST['submit'])) {
-						$selected = $_POST['spiele'];
-					}
-					echo '<option value="" disabled selected></option>';
-						add_option($selected,'s001','Konsolen Spiele');
-						add_option($selected,'s002','Online Spiele');
-						add_option($selected,'s003','PC Spiele');
-						add_option($selected,'s004','Roblox');
-					?>
+						if (isset($_POST['submit'])) {
+							$selected = $_POST['spiele'];
+						}
+						echo '<option value="" disabled selected></option>';
+							add_option($selected,'s001','Konsolen Spiele');
+							add_option($selected,'s002','Online Spiele');
+							add_option($selected,'s003','PC Spiele');
+							add_option($selected,'s004','Roblox');
+						?>
 
-		        </select>
-					<?php
-				if (isset($_POST['spiele'])) {
-					switch ($_POST['spiele']) {
-						case 's001': include ("Konsole/K.php"); break;
-						case 's002': include ("Online/O.php"); break;
-						case 's003': include ("PC/PC.php"); break;
-						case 's004': include ("Roblox/Roblox.php"); break;
-					}
-				} else {
-					include ("../../include/else.php");
-				}
-			?>
-				</div>
-			</form>
+					</select>
+						<?php
+							if (isset($_POST['spiele'])) {
+								switch ($_POST['spiele']) {
+									case 's001': include ("Konsole/K.php"); break;
+									case 's002': include ("Online/O.php"); break;
+									case 's003': include ("PC/PC.php"); break;
+									case 's004': include ("Roblox/Roblox.php"); break;
+								}
+							} else {
+								include ("../../include/else.php");
+							}
+						?>
+					</div>
+				</form>
 			</div>
                 <?php include ("../../include/footer1.php"); ?>
 </html>
