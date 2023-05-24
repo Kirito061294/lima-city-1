29816978b200684d7daa6cc3cbb24c94b626f8d1
Kirito061294 <login@mwiese.de>
2023-05-24 03:08:05 +0200

    Update visibility_switch.js

--- js/visibility_switch.js
+++ js/visibility_switch.js
@@ -6,3 +6,11 @@ function toggleGrid(id) {
   var text = document.getElementById(id);
   text.style.display = (text.style.display === "none") ? "grid" : "none";
 }
+function switch_button(id) {
+  var text = document.getElementById(id);
+  if (screen.width > 1081) {
+    text.style.display = (text.style.display === "none") ? "grid" : "none";
+  } else {
+    text.style.display = (text.style.display === "none") ? "block" : "none";
+  }
+}
