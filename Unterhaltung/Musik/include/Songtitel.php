7647b4be2268912157e9835f758d6c320e094bc1
Kirito061294 <login@mwiese.de>
2024-01-05 21:05:57 +0100

    Update Songtitel.php

--- Unterhaltung/Musik/include/Songtitel.php
+++ Unterhaltung/Musik/include/Songtitel.php
@@ -1,16 +1,24 @@
 <?php
+	$Spalte1_Tabelle = 'Interpret';
+	$Spalte1_ID = 'InterpretID';
+	$Spalte2_Tabelle = 'Kategorien';
+	$Spalte2_ID = 'catID';
+	$Spalte3_Tabelle = 'Songs';
+	$Spalte3_ID = 'SongID';
+	$Spalte3_Bezeichnung = 'SongName';
+	
 	require_once ("../../../include/data.php");
 	$connection = Connection::getInstance();
 	
 	// Bei Klick auf eine Kategorie wenn Interpret ausgewählt:
-	$query = "SELECT * FROM Songs WHERE catID =:category AND InterpretID =:interpreten ORDER BY SongName ASC";
+	$query = "SELECT * FROM $Spalte3_Tabelle WHERE $Spalte2_ID =:category AND $Spalte1_ID =:interpreten ORDER BY $Spalte3_Bezeichnung ASC";
 
 	$statement = $connection->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-	$statement->execute([":category"=>$_POST["Kategorien"],":interpreten"=>$_POST["Interpret"]]);
+	$statement->execute([":category"=>$_POST["$Spalte2_Tabelle"],":interpreten"=>$_POST["$Spalte1_Tabelle"]]);
 	$data=$statement->fetchAll();
 	$array=[];
 	foreach($data as $row) {
-		$array[]=[$row["SongID"],$row["SongName"]];
-		echo '                	<option class="kategorie03" value="' . $row['SongID'] . '">' . $row['SongName'] . '</option>'.PHP_EOL;
+		$array[]=[$row["$Spalte3_ID"],$row["$Spalte3_Bezeichnung"]];
+		echo '                	<option class="kategorie03" value="' . $row[$Spalte3_ID] . '">' . $row[$Spalte3_Bezeichnung] . '</option>'.PHP_EOL;
 	}
 ?>
