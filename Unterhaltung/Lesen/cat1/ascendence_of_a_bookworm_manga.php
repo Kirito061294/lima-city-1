7f62ffcc62879a0357732a51325215bce8895b41
Kirito061294 <login@mwiese.de>
2025-04-05 00:48:50 +0200

    Create ascendence_of_a_bookworm_manga.php

---
+++ Unterhaltung/Lesen/cat1/ascendence_of_a_bookworm_manga.php
@@ -0,0 +1,10 @@
+<div class="cat2">
+							<select name="lesen2" onchange="Lesen_2_02(this)">
+							<?php
+								$cat2_1 = '<option value="ascendence_of_a_bookworm_manga/';
+								$cat2_2 = '.php" class="kategorie02">';
+								echo '<option value="../placeholder.php" class="kategorie02">-Bitte Auswahl treffen-</option>'.PHP_EOL;
+								echo $cat2_1.'part_4/chapter-01'.$cat2_2.'Part 4 - Chapter 01</option>'.PHP_EOL;
+							?>
+							</select>
+						</div>
