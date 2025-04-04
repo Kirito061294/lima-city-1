63fb334b13e71987a5d3cebf008b050a5829a2fc
Kirito061294 <login@mwiese.de>
2025-04-05 00:46:39 +0200

    Delete Allgemeines.php

--- Themen/include/cat/Covid-19/Allgemeines.php
+++
@@ -1,5 +0,0 @@
-<p><a href="include/cat/Covid-19/0000">Allgemeine Infos</a></p>
-				    <p><a href="include/cat/Covid-19/0001">Comirnaty</a>(BioNTech) - <a href="https://de.wikipedia.org/wiki/Tozinameran"><font color="black">Wikipedia</font></a><br />
-				    <a href="include/cat/Covid-19/0002">Janssen</a>(Janssen-Cilag International NV) - <a href="https://de.wikipedia.org/wiki/Ad26.COV2.S"><font color="black">Wikipedia</font></a><br />
-				    <a href="include/cat/Covid-19/0003">Spikevax</a>(Moderna) - <a href="https://de.wikipedia.org/wiki/MRNA-1273"><font color="black">Wikipedia</font></a><br />
-				    <a href="include/cat/Covid-19/0004">Vaxzevria</a>(AstraZeneca) - <a href="https://de.wikipedia.org/wiki/AZD1222"><font color="black">Wikipedia</font></a></p>
