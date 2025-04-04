ffc82c5fe5858bac81aeed885cf9ce0e02d1b071
Kirito061294 <login@mwiese.de>
2025-04-05 00:41:03 +0200

    Create DatenschutzOnly.php

---
+++ Rechtliches/DatenschutzOnly.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <title>Datenschutz</title>
+        <?php include ("../include/head.php"); ?>
+            <style>
+				header {
+					display: none;
+				}
+				footer {
+					display: none;
+				}
+			</style>
+            <div class="center" id="main-container">
+            <div class="ueberschrift">
+                <h1>Datenschutzerklärung von <a href="https://mwiese.de" target="_blank">mwiese.de</a></h1>
+			</div>
+            <div class="main_content" style="height: 30em; overflow-y: scroll;">
+				<?php include ("Datenschutz1.php") ?>
+            </div>
+			</div>
+                <?php include ("../include/footer.php"); ?>
+</html>
