73b4e24324ce03a8fbeb2b0914b9ba0c14a08f7f
Kirito061294 <login@mwiese.de>
2025-04-05 00:33:19 +0200

    Create Gott.php

---
+++ Glaube/themen/cat1/Gott.php
@@ -0,0 +1,10 @@
+<div class="cat2">
+							<select name="themen2" onchange="Themen_02(this)">
+							<?php
+								$cat1_1 = '<option value="';
+								$cat1_2 = '.php" class="kategorie01">';
+								echo '<option value="placeholder'.$cat1_2.'-Bitte Auswahl treffen-</option>'.PHP_EOL;
+								echo $cat1_1.'Gottes_Zusagen'.$cat1_2.'Gottes Zusagen</option>'.PHP_EOL;
+							?>
+							</select>
+						</div>
