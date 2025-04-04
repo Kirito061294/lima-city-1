22819d2d8834ce3fe5f30664436f5f4645f2e0f2
Kirito061294 <login@mwiese.de>
2025-04-05 00:44:02 +0200

    Update OS.php

--- Technik/OS.php
+++ Technik/OS.php
@@ -1,49 +1,39 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-       "http://www.w3.org/TR/html4/loose.dtd">
-
-
-<html>
-    <head>
-        <title>Betriebssysteme</title>
-        <?php include ("../include/head1.php"); ?>
-            <div class="ueberschrift">
+<link rel="stylesheet" type="text/css" href="https://mwiese.de/include/css/auswahlmenue.css">
+			<div class="ueberschrift">
                 <h1>Betriebssysteme</h1>
 			</div>
-            <div class="main_content">
-			<form action="" method="post" class="mb-3">
-				<div><input type="submit" name="submit" value="Auswahl"></div>
-				<div class="include1">
-			    <select name="os">
-					<?php
-					$selected = '';
-					function add_option($selected, $option_value,$interpret_name) {
-						echo '<option value="'.$option_value.'"';
-						if ($selected == $option_value) {
-							echo ' selected="selected"';
-						}
-						echo '>'.$interpret_name.'</option>';
-					}
-					if (isset($_POST['submit'])) {
-						$selected = $_POST['os'];
-					}
-					echo '<option value="" disabled selected></option>';
-						add_option($selected,'os001','Linux');
-						add_option($selected,'os002','Microsoft Windows');
-					?>
-
-		        </select>
-					<?php
-						if (isset($_POST['os'])) {
-							switch ($_POST['os']) {
-								//case 'os001': include ("OS/Linux.php"); break;
-								case 'os002': include ("OS/Win.php"); break;
-							}
-						} else {
-							include ("../include/else.php");
-						}
-					?>
+			<div class="main_content">
+				<div><button class="menue_switch mobile" onclick="toggleBlock('auswahlmenue')">Menü ein-/ausblenden</button></div>
+				<div id="auswahlmenue">
+					<div class="spalte1">
+						<div>
+							<select name="OS" onChange="OS_01(this)">
+								<?php
+									$Spalte1_Tabelle = 'Tabelle_OS_01';
+									$Spalte1_ID = 'OS_01_ID';
+									$Spalte1_Bezeichnung = 'OS_01_Name';
+	
+									require_once ("../include/data.php");
+									//  Für Interpretenliste
+									$selected = '';
+									
+									$connection = Connection::getInstance();
+									$query = "SELECT * FROM $Spalte1_Tabelle";
+			
+									$statement = $connection->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+									$statement->execute();
+					
+									echo ''.PHP_EOL;
+									while ($row = $statement->fetch()) {
+										echo '					<option value="' . $row[$Spalte1_ID] . '" ';
+										echo 'class="kategorie01">' . $row[$Spalte1_Bezeichnung] . '</option>'.PHP_EOL;
+									}
+								?>
+							</select>
+						</div>
+					</div>
+					<div class="spalte2"><div class="cat2"><select id="output" onChange="OS_02(this)"></select></div></div>
+					<div class="spalte3"><div class="cat3"><select id="output2" onChange="OS_03(this)"></select></div></div>
 				</div>
-            </form>
-			</div>
-                <?php include ("../include/footer1.php"); ?>
-</html>
+				<div id="ausgabe"></div>
+            </div>
