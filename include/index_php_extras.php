f08a88cad0b967c03ee0502ef62339d1a536e51f
Kirito061294 <login@mwiese.de>
2025-04-05 00:35:48 +0200

    Create index_php_extras.php

---
+++ include/index_php_extras.php
@@ -0,0 +1,13 @@
+<?php
+$home = 'https://mwiese.de';
+$tab4 = '				';
+$tab5 = '					';
+$checkbox_p1 = '						<input type="checkbox" id="';
+$checkbox_p2 = '_checkbox">'.PHP_EOL.'						<label for="';
+$ul2_start = '						<ul class="dropdown">';
+$ul2_end = '						</ul>';
+$li1_start = '					<li class="expandable_li">';
+$li1_end = '					</li>';
+$li2_p1 = '							<li><a onclick="loadPage';
+$li2_p2 = ';" target="main">';
+?>
