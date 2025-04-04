f937f9c899269b9159799b844ac0491e7f288da2
Kirito061294 <login@mwiese.de>
2025-04-05 00:41:50 +0200

    Create ImpressumOnly.php

---
+++ Rechtliches/ImpressumOnly.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "https://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <title>Impressum</title>
+		<?php include ("../include/head.php"); ?>
+            <style>
+				header {
+					display: none;
+				}
+				footer {
+					display: none;
+				}
+			</style>
+            <div class="center" id="main-container">
+            <div class="ueberschrift">
+                <h1>Impressum von <a href="https://mwiese.de" target="_blank">mwiese.de</a></h1>
+			</div>
+			<?php include ("Impressum1.php"); ?>
+			</div>
+			<?php include ("../include/footer.php"); ?>
+</html>
