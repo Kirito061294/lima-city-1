f45418674efe5fdba365d5552ac4e354a3053d0a
Kirito061294 <login@mwiese.de>
2025-04-05 00:36:52 +0200

    Create open_head.php

---
+++ include/open_head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<title>mwiese.de</title>
+		<meta charset="UTF-8" lang="de" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+		<meta name="description" content="Autor: Marcel Wiese. Copyright: 2018">
+		<link rel="shortcut icon" href="https://mwiese.de/include/45454.ico" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="https://mwiese.de/include/min-style.css">
