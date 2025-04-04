afd256f07dfc24cbb2f8a4994b795d12745df8b3
Kirito061294 <login@mwiese.de>
2025-04-05 00:35:12 +0200

    Create default-loaded.php

---
+++ include/default-loaded.php
@@ -0,0 +1,9 @@
+<div class="ueberschrift">
+			    <h1 style="font-size: 150%;">Herzlich Wilkommen auf mwiese.de</h1>
+			</div>
+			<div id="info" style="display: block;">
+                <p>Diese Seite ist ein Hobby und daher kann es sein das es dauert bis neues hinzugefügt wird.</p>
+				<p>Zusätzlich sei gesagt, das diese Website von sich aus keine Browserdaten der Nutzer erhebt.</p>
+				<p>Das einzige was der Provider dieser Domain (die TrafficPlex GmbH) an Daten erhebt ist die anzahl der einzelnen Seitenaufrufe.</p>
+				<p>Sollten auf nachfolgenden Seiten anderweitig Daten erhoben werden, wird darauf hingewiesen.</p>
+			</div>
