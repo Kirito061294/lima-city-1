31e95fe23db3baa2b6724bd3d09b36486fb0026f
Kirito061294 <login@mwiese.de>
2025-04-05 00:33:22 +0200

    Create Jesus.php

---
+++ Glaube/themen/cat1/Jesus.php
@@ -0,0 +1,10 @@
+<div class="cat2">
+							<select name="themen2" onchange="Themen_02(this)">
+							<?php
+								$cat1_1 = '<option value="Jesus/';
+								$cat1_2 = '.php" class="kategorie01">';
+								echo '<option value="placeholder'.$cat1_2.'-Bitte Auswahl treffen-</option>'.PHP_EOL;
+								echo $cat1_1.'Jesus_im_Alten_Testament'.$cat1_2.'Jesus im Alten Testament</option>'.PHP_EOL;
+							?>
+							</select>
+						</div>
