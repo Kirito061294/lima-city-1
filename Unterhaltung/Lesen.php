f42ffeaf52a7b84fbf09a7601e021a643030fb24
Kirito061294 <login@mwiese.de>
2025-04-05 00:48:25 +0200

    Update Lesen.php

--- Unterhaltung/Lesen.php
+++ Unterhaltung/Lesen.php
@@ -1,56 +1,33 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-       "http://www.w3.org/TR/html4/loose.dtd">
-
-
-<html>
-    <head>
-        <title>Manga</title>
-        <?php include ("../include/head1.php"); ?>
-			<link rel="stylesheet" type="text/css" href="Lesen/Lesen.css">
+<?php include ("unterhaltung_extras.php");
+echo $lesen_css; ?>
             <div class="ueberschrift">
-                <h1>Manga</h1>
+                <h1>Lesen</h1>
 			</div>
-			<button class="knopf" onclick="toggleBlock('info')">Info</button>
-			<div id="info">
-				<p>Die hier verfügbaren Inhalte wurden vom Betreiber dieser Seite mit dem Google Übersetzer übersetzt, Fehler sind hierbei möglich.</p>
-			</div>
-            <div class="main_content">
-				<script src="https://mwiese.de/js/jquery.js"></script>
-				<div><button class="menue_switch mobile" onclick="toggleBlock('auswahlmenue')">Menü ein-/ausblenden</button></div>
-				<div id="auswahlmenue">
-					<link rel="stylesheet" type="text/css" href="https://mwiese.de/include/css/auswahlmenue.css">
-					<script src="Lesen/auswahlmenue.js"></script>
-					<div class="spalte1">
-						<div>
-							<select name="Lesen" onChange="Lesen_01(this)">
-								<?php
-									$Spalte1_Tabelle = 'Tabelle_Lesen_01';
-									$Spalte1_ID = 'Lesen_01_ID';
-									$Spalte1_Bezeichnung = 'Lesen_01_Name';
-	
-									require_once ("../include/data.php");
-									//  Für Interpretenliste
-									$selected = '';
-									
-									$connection = Connection::getInstance();
-									$query = "SELECT * FROM $Spalte1_Tabelle WHERE visible = '1'";
-			
-									$statement = $connection->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-									$statement->execute();
-					
-									echo ''.PHP_EOL;
-									while ($row = $statement->fetch()) {
-										echo '					<option value="' . $row[$Spalte1_ID] . '" ';
-										echo 'class="kategorie01">' . $row[$Spalte1_Bezeichnung] . '</option>'.PHP_EOL;
-									}
-								?>
-							</select>
-						</div>
-					</div>
-					<div class="spalte2"><div class="cat2"><select id="output" name="variant" onChange="Lesen_02(this)"></select></div></div>
-					<div class="spalte3"><div class="cat3"><select id="output2" name="chapter" onChange="Lesen_03(this)"></select></div></div>
+			<div style="display: grid;grid-template-columns: 1fr 1fr;">
+				<div class="spalte1" style="border: 0;">
+					<button class="knopf" onclick="toggleBlock('info')">Info</button>
 				</div>
-				<div id="ausgabe"></div>
-            </div>
-			<?php include ("../include/footer1.php"); ?>
-</html>
+				<div class="spalte2" style="border: 0;">
+					<button class="knopf" onclick="toggleBlocks('chapter_images'); toggleBlocks('chapter_images_alternative');">Bilder aus/einblenden</button>
+				</div>
+			</div>
+			<div id="info">
+				<p>Die hier verfügbaren Manga wurden vom Betreiber dieser Seite mit dem Google Übersetzer übersetzt, Fehler sind hierbei möglich.</p>
+			</div>
+            <?php echo $unterhaltung_main_content; ?>
+							<select name="lesen1" onchange="Lesen_2_01(this)">
+							<?php
+								$cat1_1 = '<option value="';
+								$cat1_2 = '.php" class="kategorie01">';
+								echo '<option value="placeholder'.$cat1_2.'-Bitte Auswahl treffen-</option>'.PHP_EOL;
+								#echo $cat1_1.'a_real_goddess_would_let_nobody_die'.$cat1_2.'A real Goddess would let nobody die</option>'.PHP_EOL;
+								#echo $cat1_1.'ascendence_of_a_bookworm_manga'.$cat1_2.'Ascendence of a Bookworm - Manga</option>'.PHP_EOL;
+								echo $cat1_1.'Boosted_restart'.$cat1_2.'Boosted restart</option>'.PHP_EOL;
+								#echo $cat1_1.'Boosted_continuation'.$cat1_2.'Boosted Continuation</option>'.PHP_EOL;
+								#echo $cat1_1.'Heaven_Earth_Me'.$cat1_2.'Heaven. Earth. Me.</option>'.PHP_EOL;
+								#echo $cat1_1.'I_was_betrayed_then_reincarnated_as_a_Kitsune'.$cat1_2.'I was betrayed then reincarnated as a Kitsune</option>'.PHP_EOL;
+								#echo $cat1_1.'Reborn_from_the_Cosmos'.$cat1_2.'Reborn from the Cosmos</option>'.PHP_EOL;
+								#echo $cat1_1.'The_red_Hand'.$cat1_2.'The red Hand</option>'.PHP_EOL;
+							?>
+							</select>
+						<?php echo $unterhaltung_main_content_ende; ?>
