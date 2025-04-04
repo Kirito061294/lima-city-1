bb7aa9054918f85f95bd62cb1be1775f50e9ec73
Kirito061294 <login@mwiese.de>
2025-04-05 00:32:09 +0200

    Create Hinweise.php

---
+++ Glaube/Bibel/Hinweise.php
@@ -0,0 +1,8 @@
+<div class="ueberschrift">
+                <h1>Hinweise</h1>
+			</div>
+            <div class="main_content">
+				<button class="knopf"><a onclick="loadPage2('Bibel/hw/fue.php');" target="main">Fehlerhafte Übersetzungen</a></button>
+				<div id="container2">
+				</div>
+            </div>
