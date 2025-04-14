455559760bf5c63af66556775526044483f291d8
Kirito061294 <login@mwiese.de>
2025-04-14 21:52:37 +0200

    Delete Test1.php

--- Testordner/Tests/Test1.php
+++
@@ -1,6 +0,0 @@
-<div style="height: 25em; overflow-y: scroll;">
-									<div class ="verse">
-										<div class="zitat1"><p><b>Test-Überschrift</p></b><p>Test-Absatz</p></div>
-										<div class="kommentar1"><p>Test</p></div>
-									</div>
-								</div>
