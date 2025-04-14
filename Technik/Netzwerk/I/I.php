cd84278a157ee7b07783850aa497eb342d7a9822
Kirito061294 <login@mwiese.de>
2025-04-14 21:49:39 +0200

    Delete I.php

--- Technik/Netzwerk/I/I.php
+++
@@ -1,2 +0,0 @@
-<p>Die Links hier verweisen auf externe Seiten. mwiese.de hat nichts mit selbigen zu tun.</p>
-<p><a href="http://www.dein-ip-check.de/" target="_blank">IP-Test</a> - (by dein-ip-check.de)</p>
